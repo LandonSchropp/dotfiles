4d3860ea0011a3a15c01140986a37f7fb29ffe6a
Landon Schropp <schroppl@gmail.com>
2012-10-08 22:46:28 -0700

    Cleaning up and organizing.

--- settings/oh_my_zsh/zsh.zsh
+++ settings/oh_my_zsh/zsh.zsh
@@ -1,15 +1,10 @@
-# sublime text aliases
-alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+# start up rbenv
+eval "$(rbenv init -)"
 
-#rails aliases
-alias grd='bundle exec guard --notify false'
-alias migrate='bundle exec rake db:migrate && bundle exec rake db:test:prepare'
-alias populate='bundle exec rake db:populate'
+# set up NodeJS environment
+export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules"
 
-# git aliases
-alias gitc='git add . && git commit -a -m '
-
-# shortcuts
+# directory aliases
 alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
@@ -19,18 +14,24 @@ alias -g ........='../../../../../../..'
 alias -g .........='../../../../../../../..'
 alias -g ..........='../../../../../../../../..'
 
-alias -g cur='~/Development/current'
+alias cur='cd ~/Development/current'
 
-# This was taken from: http://www.askdavetaylor.com/how_do_i_figure_out_my_ip_address_on_a_mac.html
-alias -g ip='ifconfig en1 | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2'
+# git aliases
+alias gitc='git add . && git commit -a -m '
+
+# sublime text aliases
+alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+
+#rails aliases
+alias grd='bundle exec guard --notify false'
+alias migrate='bundle exec rake db:migrate && bundle exec rake db:test:prepare'
+alias populate='bundle exec rake db:populate'
 
 # alias the terminal commands
-alias -g ter2='macruby ~/toolbox/open_terminal_windows.rb 2'
-alias -g ter3='macruby ~/toolbox/open_terminal_windows.rb 3'
-alias -g ter4='macruby ~/toolbox/open_terminal_windows.rb 4'
+alias -g ter2='macruby ~/toolbox/utilities/open_terminal_windows.rb 2'
+alias -g ter3='macruby ~/toolbox/utilities/open_terminal_windows.rb 3'
+alias -g ter4='macruby ~/toolbox/utilities/open_terminal_windows.rb 4'
 
-# start up rbenv
-eval "$(rbenv init -)"
-
-# set up NodeJS environment
-export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules"
+# Prints the current IP address.
+# Taken from: http://www.askdavetaylor.com/how_do_i_figure_out_my_ip_address_on_a_mac.html
+alias -g ip='ifconfig en1 | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2'
--- setup/ruby_on_rails.rb
+++ setup/ruby_on_rails.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 
 # Installs Rails.
+#
+# Prerequisits:
+# * GCC 4.2
+# * Ruby
 
 `gem install sqlite3`
 `gem install rails`
