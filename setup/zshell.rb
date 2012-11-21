2616ca72dcd885d19b667cbc93b2732d1c0774f5
Landon Schropp <schroppl@gmail.com>
2012-11-20 23:28:43 -0800

    Fixed bugs in zshell script.

--- setup/zshell.rb
+++ setup/zshell.rb
@@ -7,8 +7,8 @@ require 'pathname'
 
 # define the directories
 FILE_DIRECTORY = Pathname.new(File.dirname(__FILE__)).realpath.to_s 
-SETTINGS_DIRECTORY = "#{FILE_DIRECTORY}/settings"
-OH_MY_ZSH_DIRECTORY = Pathname.new(File.expand_path('~/.oh-my-zsh')).realpath.to_s
+SETTINGS_DIRECTORY = Pathname.new(File.expand_path("#{FILE_DIRECTORY}/../settings")).to_s
+OH_MY_ZSH_DIRECTORY = Pathname.new(File.expand_path('~/.oh-my-zsh')).to_s
 OH_MY_ZSH_CUSTOM_DIRECTORY = "#{OH_MY_ZSH_DIRECTORY}/custom"
 
 # install oh-my-zsh
@@ -18,5 +18,9 @@ if File.exists?(OH_MY_ZSH_CUSTOM_DIRECTORY) or File.symlink?(OH_MY_ZSH_CUSTOM_DI
 end
 
 # symbolically link the directories
-`ln -s #{SETTINGS_DIRECTORY + "/oh_my_zsh"} #{OH_MY_ZSH_CUSTOM_DIRECTORY}`
+FileUtils.mkdir_p OH_MY_ZSH_CUSTOM_DIRECTORY
+`ln -s #{SETTINGS_DIRECTORY + "/oh_my_zsh/zsh.zsh"} #{OH_MY_ZSH_CUSTOM_DIRECTORY}`
+
+# start zshell
+`/bin/zsh`
 `source ~/.zshrc`
