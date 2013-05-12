e1c86bb7f51484d6e15c840c326c2160b9b6717e
Landon Schropp <schroppl@gmail.com>
2013-05-12 13:42:58 -0700

    Updated the git aliases.

--- oh_my_zsh/git.zsh
+++ oh_my_zsh/git.zsh
@@ -1,7 +1,7 @@
 # A set of aliases and functions for working with git.
 
 # Add all of the files in the current directory and commit them with the provided message.
-alias gitc='git add . && git commit -a -m '
+alias gc='git add . && git commit -a -m '
 
 # Display a compact, colorized git log. Taken from: http://alias.sh/compact-colorized-git-log
 alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
