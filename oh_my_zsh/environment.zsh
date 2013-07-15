436d4a6cffe858145859a842c3fa41a233efe6db
Landon Schropp <schroppl@gmail.com>
2013-07-15 15:17:16 -0700

    Added the node modules install directory to the PATH.

--- oh_my_zsh/environment.zsh
+++ oh_my_zsh/environment.zsh
@@ -1,5 +1,5 @@
 # set up NodeJS environment
-export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules"
+export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules:/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules"
 
 # set the default text editor
 export EDITOR="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
@@ -9,7 +9,7 @@ rm -f /usr/local/bin/subl
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 
 # set up the path so brew programs override local programs
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/share/npm/bin:$PATH
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
 
 # start up rbenv
 eval "$(rbenv init -)"
