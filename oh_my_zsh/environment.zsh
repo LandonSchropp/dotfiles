09d28d9c7540138c671420f37051e82f4248ebb6
Landon Schropp <schroppl@gmail.com>
2013-11-20 22:52:50 -0800

    Updated the EDITOR environment variable.

--- oh_my_zsh/environment.zsh
+++ oh_my_zsh/environment.zsh
@@ -1,12 +1,12 @@
 # set up NodeJS environment
 export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules:/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules"
 
-# set the default text editor
-export EDITOR="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
-
 # sublime text symbolic link
 rm -f /usr/local/bin/subl
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+# set the default text editor
+export EDITOR="/usr/local/bin/subl"
 
 # set up the path so brew programs override local programs
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$NODE_PATH:$PATH
