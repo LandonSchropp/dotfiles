166666a77f75dc424bf72c8da375b52b10450bbe
Landon Schropp <schroppl@gmail.com>
2015-04-25 11:16:11 -0700

    Dumped pyenv

--- oh_my_zsh/environment.zsh
+++ oh_my_zsh/environment.zsh
@@ -4,9 +4,8 @@ export EDITOR="/usr/local/bin/atom"
 # set the lolcommits delay
 export LOLCOMMITS_DELAY=1
 
-# start up rbenv and pyenv
+# start up rbenv
 eval "$(rbenv init -)"
-eval "$(pyenv init -)"
 
 # set up the local bin path
 export PATH="/usr/local/heroku/bin:$PATH"
