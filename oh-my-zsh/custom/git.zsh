4798ed86521b1216e6e9fdddadffec5662a69675
Landon Schropp <schroppl@gmail.com>
2016-04-21 13:02:54 -0700

    Add an alias for git rebase -i master

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,4 +1,5 @@
 alias git_delete_merged_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias git_rebase_master='git rebase -i master'
 
 git_autosquash() {
   git rebase --autosquash -i $(git merge-base head $1);
