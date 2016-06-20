2862f4ed51594107ad548f8df3c4ae2266cf769d
Landon Schropp <schroppl@gmail.com>
2016-06-19 17:24:27 -0700

    Added alias for what I did today

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,6 +1,8 @@
 alias git_delete_merged_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias git_rebase_master='git rebase -i master'
 
+alias git_what_i_did_today='git log --branches --remotes --tags --graph --oneline --author="Landon" --since="6am"'
+
 git_autosquash() {
   git rebase --autosquash -i $(git merge-base head $1);
 }
