92c91447432b376cd229fb23818a6a2b29e1ed68
Landon Schropp <schroppl@gmail.com>
2016-07-04 12:42:36 -0700

    Fix git_delete_merged_branches

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,7 +1,14 @@
-alias git_delete_merged_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-alias git_rebase_master='git rebase -i master'
+git_delete_merged_branches() {
+  git branch --no-color --merged | grep -v "\*" | xargs -n 1 git branch -d
+}
 
-alias git_what_i_did_today='git log --branches --remotes --tags --graph --oneline --author="Landon" --since="6am"'
+git_rebase_master() {
+  git rebase -i master
+}
+
+git_what_i_did_today() {
+  git log --branches --remotes --tags --graph --oneline --author="Landon" --since="6am"
+}
 
 git_autosquash() {
   git rebase --autosquash -i $(git merge-base head $1);
