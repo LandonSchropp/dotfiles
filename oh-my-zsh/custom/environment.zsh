86d86136c23cd8cf5714c3e4035e9e2bd88b8616
Landon Schropp <schroppl@gmail.com>
2016-10-02 22:57:29 -0700

    Remove rbenv and direnv from the environment config

--- oh-my-zsh/custom/environment.zsh
+++ oh-my-zsh/custom/environment.zsh
@@ -1,17 +1,8 @@
 # set the default text editor
 export EDITOR="nvim"
 
-# start up rbenv
-eval "$(rbenv init -)"
-
-# start up direnv
-eval "$(direnv hook zsh)"
-
 # set up the local bin paths
 export PATH="./bin:./node_modules/.bin:/usr/local/heroku/bin:$PATH"
-
-# set up andoird path
-export ANDROID_HOME=/usr/local/opt/android-sdk
 
 # Remove the delay when hitting the escape key
 export KEYTIMEOUT=1
