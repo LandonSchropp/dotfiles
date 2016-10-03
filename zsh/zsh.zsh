b6f4cb3bbf96cb7a06b132facc1647a5a9098535
Landon Schropp <schroppl@gmail.com>
2016-10-02 23:01:27 -0700

    Remove unnecessary config

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -1,7 +1,2 @@
-# Configure ZSH.
-
 # Disable automatically setting the title.
 export DISABLE_AUTO_TITLE="true"
-
-# Prevent Oh my ZSH from prompting before updating itself.
-export DISABLE_UPDATE_PROMPT="true"
