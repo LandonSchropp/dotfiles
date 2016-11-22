0e0d8f7405eeead5fae40b682da9a67ffdcef29e
Landon Schropp <schroppl@gmail.com>
2016-11-22 11:55:39 -0800

    Improve docker-clean script
    This prevents the commands from running if there's nothing for them to
    run on.

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,3 +1,27 @@
 alias -g dc='docker-compose'
 alias -g dcr='docker-compose run'
 alias -g dcu='docker-compose up'
+
+# Taken from: https://www.calazan.com/docker-cleanup-commands/
+function docker-clean {
+  RUNNING_CONTAINERS=$(docker ps -q)
+
+  if [[ ! -z $RUNNING_CONTAINERS ]]; then
+    printf "\nStopping running containers\n\n"
+    docker stop $RUNNING_CONTAINERS
+  fi
+
+  STOPPED_CONTAINERS=$(docker ps -a -q)
+
+  if [[ ! -z $STOPPED_CONTAINERS ]]; then
+    printf "\nDeleting stopped containers\n\n"
+    docker rm $STOPPED_CONTAINERS
+  fi
+
+  UNTAGGED_IMAGES=$(docker images -q -f dangling=true)
+
+  if [[ ! -z $UNTAGGED_IMAGES ]]; then
+    printf "\nDeleting untagged images\n\n"
+    docker rmi $UNTAGGED_IMAGES
+  fi
+}
