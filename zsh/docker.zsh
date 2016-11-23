af0192b657b3de459579c866e5791f81c6f93e52
Landon Schropp <schroppl@gmail.com>
2016-11-23 14:25:40 -0800

    Add separate function for docker clean all

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -3,25 +3,37 @@ alias -g dcr='docker-compose run'
 alias -g dcu='docker-compose up'
 
 # Taken from: https://www.calazan.com/docker-cleanup-commands/
-function docker-clean {
+function docker-clean-dangling {
   RUNNING_CONTAINERS=$(docker ps -q)
 
   if [[ ! -z $RUNNING_CONTAINERS ]]; then
     printf "\nStopping running containers\n\n"
-    docker stop $RUNNING_CONTAINERS
+    docker stop $(echo $RUNNING_CONTAINERS)
   fi
 
   STOPPED_CONTAINERS=$(docker ps -a -q)
 
   if [[ ! -z $STOPPED_CONTAINERS ]]; then
     printf "\nDeleting stopped containers\n\n"
-    docker rm $STOPPED_CONTAINERS
+    docker rm $(echo $STOPPED_CONTAINERS)
   fi
 
-  UNTAGGED_IMAGES=$(docker images -q -f dangling=true)
+  DANGLING_IMAGES=$(docker images -q -f dangling=true)
 
-  if [[ ! -z $UNTAGGED_IMAGES ]]; then
-    printf "\nDeleting untagged images\n\n"
-    docker rmi $UNTAGGED_IMAGES
+  if [[ ! -z $DANGLING_IMAGES ]]; then
+    printf "\nDeleting dangling images\n\n"
+    docker rmi $(echo $DANGLING_IMAGES)
   fi
+}
+
+function docker-clean {
+  docker-clean-dangling
+
+  ALL_IMAGES=$(docker images -q -a)
+
+  if [[ ! -z $ALL_IMAGES ]]; then
+    printf "\nDeleting all images\n\n"
+    docker rmi $(echo $ALL_IMAGES)
+  fi
+
 }
