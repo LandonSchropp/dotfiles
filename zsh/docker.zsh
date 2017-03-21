bd4bff2bf40eeb25273132b408f4fa43d548ea23
Landon Schropp <schroppl@gmail.com>
2017-03-21 11:43:04 -0700

    Automatically remove containers created by dcr
    This prevents a bunch of detached containers from hanging around.

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,5 +1,5 @@
 alias -g dc='docker-compose'
-alias -g dcr='docker-compose run'
+alias -g dcr='docker-compose run --rm'
 alias -g dcu='docker-compose up -d'
 alias -g dcl='docker-compose logs -f'
 alias -g dcb='docker-compose build'
