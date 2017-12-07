fffacb267bade1dde6362c29f7e5f42f90c4a637
Landon Schropp <schroppl@gmail.com>
2017-12-06 18:10:48 -0800

    Add automatic project names to docker commands

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,7 +1,13 @@
+# Ignore project names that aren't prepended with `tesla` or `edison`.
+# TODO: Genericize this.
+project_name() {
+  [[ $(basename `pwd`) =~ '(tesla|edison)-(.*)' ]] && echo $match[2] | sed 's/-/_/g'
+}
+
 alias -g dc='docker-compose'
-alias -g dcr='docker-compose run --rm'
+alias -g dcr='docker-compose run --rm $(project_name)'
 alias -g dcu='docker-compose up -d'
-alias -g dcl='docker-compose logs -f'
-alias -g dcb='docker-compose build'
+alias -g dcl='docker-compose logs -f $(project_name)'
+alias -g dcb='docker-compose build $(project_name)'
 
 alias -g docker-kill='pgrep -i -f docker | xargs kill'
