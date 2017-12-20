b717a248e50a13b090e05a8fd039c7aec0c44bac
Landon Schropp <schroppl@gmail.com>
2017-12-20 10:24:57 -0800

    Add fish theme

---
+++ config/fish/config/theme.fish
@@ -0,0 +1,27 @@
+# TODO: Set these colors to match the underlying term colors.
+set -g fish_color_autosuggestion 969896
+set -g fish_color_cancel -r
+set -g fish_color_command c397d8
+set -g fish_color_comment e7c547
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end c397d8
+set -g fish_color_error d54e53
+set -g fish_color_escape 'bryellow' '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param 7aa6da
+set -g fish_color_quote b9ca4a
+set -g fish_color_redirection 70c0b1
+set -g fish_color_search_match 'bryellow' '--background=brblack'
+set -g fish_color_selection 'white' '--bold' '--background=brblack'
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion
+set -g fish_pager_color_description 'B3A06D'  'yellow'
+set -g fish_pager_color_prefix 'white' '--bold' '--underline'
+set -g fish_pager_color_progress 'brwhite' '--background=cyan'
