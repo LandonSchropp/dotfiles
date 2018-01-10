044e5fcf46609f0dd0bec4773bb9491d1c9d8ae6
Landon Schropp <schroppl@gmail.com>
2018-01-10 10:25:31 -0800

    Add command timer function

---
+++ config/fish/config/commamd_timer.fish
@@ -0,0 +1,24 @@
+function pluralize
+  printf "%d %s" $argv[2] $argv[1]
+
+  if test $argv[2] -ge 0
+    printf  "s"
+  end
+end
+
+
+function command_timer --on-event fish_postexec
+  set -l duration $CMD_DURATION
+
+  if test $duration -lt 10000
+    return
+  end
+
+  set -l hours (pluralize hour (math "$duration / 1000 / 60 / 60"))
+  set -l minutes (pluralize minute (math "$duration / 1000 / 60 % 60"))
+  set -l seconds (pluralize second (math "$duration / 1000 % 60"))
+
+  set_color blue
+
+  printf "🕐  %s, %s, %s" $hours $minutes $seconds
+end
