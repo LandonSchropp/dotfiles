d8fff8830b49a36dd073119753332e73ed9ae282
Landon Schropp <schroppl@gmail.com>
2018-07-03 13:13:39 -0700

    Make all hooks executable

--- hooks/pre-up-hooks/homebrew.sh
+++ hooks/pre-up-hooks/homebrew.sh
@@ -3,9 +3,10 @@
 set -euo pipefail
 
 # Install Homebrew if it has not already been installed
-if ! command -v base16-builder 2>/dev/null; then
+if ! command -v base16-builder 1>/dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Install all of the Homebrew applications
-brew bundle --file="$HOME/.dotfiles/Brewfile"
+# BUG FIX: This command needs to be forced to exit 0 because some apps may already be installed.
+brew bundle --file="$HOME/.dotfiles/Brewfile" || :
