5f29ee2ada04fd1dd5a225486e9d964e266f374b
Landon Schropp <schroppl@gmail.com>
2018-07-03 12:16:42 -0700

    Consolidate the Vim installation hook

--- hooks/post-up-hooks/vim_spellfile.sh
+++
@@ -1,5 +0,0 @@
-# Symbolically link the system dictionary to the vim spellfile
-mkdir -p $HOME/Library/Spelling
-mkdir -p $HOME/.config/nvim/spelling
-ln -sf $HOME/Dropbox/Mackup/Spelling/LocalDictionary $HOME/Library/Spelling/LocalDictionary
-ln -sf $HOME/Dropbox/Mackup/Spelling/LocalDictionary $HOME/.config/nvim/spelling/en.utf-8.add
