66b7052ad4534d9ed72a8d8c3be2880a2f8c8791
Landon Schropp <schroppl@gmail.com>
2018-07-03 12:03:12 -0700

    Check to see if Homebrew is installed first

--- hooks/pre-up-hooks/homebrew.sh
+++ hooks/pre-up-hooks/homebrew.sh
@@ -1,5 +1,7 @@
 # Install Homebrew if it has not already been installed
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! command -v base16-builder 2>/dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Install all of the Homebrew applications
 brew bundle --file="$HOME/.dotfiles/Brewfile"
