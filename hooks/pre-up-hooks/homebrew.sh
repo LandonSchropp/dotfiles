f45013df8ded764f48d0d04752b1e89d3f48270e
Landon Schropp <schroppl@gmail.com>
2018-07-03 10:34:48 -0700

    Appease shellcheck

--- hooks/pre-up-hooks/homebrew.sh
+++ hooks/pre-up-hooks/homebrew.sh
@@ -2,4 +2,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install all of the Homebrew applications
-brew bundle --file=$HOME/.dotfiles/Brewfile
+brew bundle --file="$HOME/.dotfiles/Brewfile"
