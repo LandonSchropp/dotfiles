aa0c09b8b5582b7c9b9c5aef387f8619826d6c9e
Landon Schropp <schroppl@gmail.com>
2018-12-05 13:33:06 -0800

    Fix a typo in the Homebrew set up

--- hooks/pre-up-hooks/homebrew.sh
+++ hooks/pre-up-hooks/homebrew.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Install Homebrew if it has not already been installed
-if ! command -v base16-builder 1>/dev/null; then
+if ! command -v brew 1>/dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
