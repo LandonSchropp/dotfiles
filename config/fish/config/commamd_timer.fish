63e59fda1ac537ed762fa6884aecccb84014f2c8
Landon Schropp <schroppl@gmail.com>
2019-01-09 15:38:37 -0800

    Fix a bug in the command timer
    As of Fish 3.0, Math no longer truncates integer division by default.

--- config/fish/config/commamd_timer.fish
+++ config/fish/config/commamd_timer.fish
@@ -2,7 +2,7 @@ function pluralize
   printf "%d %s" $argv[2] $argv[1]
 
   if test $argv[2] -ge 0
-    printf  "s"
+    printf "s"
   end
 end
 
@@ -14,9 +14,9 @@ function command_timer --on-event fish_postexec
     return
   end
 
-  set -l hours (pluralize hour (math "$duration / 1000 / 60 / 60"))
-  set -l minutes (pluralize minute (math "$duration / 1000 / 60 % 60"))
-  set -l seconds (pluralize second (math "$duration / 1000 % 60"))
+  set -l hours (pluralize hour (math --scale 0 "$duration / 1000 / 60 / 60"))
+  set -l minutes (pluralize minute (math --scale 0 "$duration / 1000 / 60 % 60"))
+  set -l seconds (pluralize second (math --scale 0 "$duration / 1000 % 60"))
 
   set_color blue
 
