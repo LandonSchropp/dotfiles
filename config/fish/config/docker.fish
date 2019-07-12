d9e24c4c883ee499fb7fe0aa18111f0c875f9496
Landon Schropp <schroppl@gmail.com>
2019-07-12 11:26:46 -0700

    Add docker-nuke command

--- config/fish/config/docker.fish
+++ config/fish/config/docker.fish
@@ -4,16 +4,19 @@ function project_name
   string match -r '(tesla|edison)-(.+)' (basename (pwd)) | sed -n 3p | sed 's/-/_/g'
 end
 
+alias k='kubectl'
 alias dc="docker-compose"
 alias dcr="docker-compose run --rm (project_name)"
 alias dcu="docker-compose up -d"
 alias dcl="docker-compose logs -f (project_name)"
 alias dcb="docker-compose build (project_name)"
-alias dco="nvim ~/Development/tesla/docker-compose.override.yml"
 
 # TODO: Genericize this.
 alias dco="$EDITOR ~/Development/tesla/docker-compose.override.yml"
 
 alias docker-kill='pgrep -i -f docker | xargs kill'
 
-alias k='kubectl'
+function docker-nuke
+  docker-compose down -v --rmi all --remove-orphans
+  docker system prune --all --force --volumes
+end
