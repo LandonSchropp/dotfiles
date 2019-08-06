a5eff8b78fa6346ddf996285ed071396cc5d98f7
Landon Schropp <schroppl@gmail.com>
2019-08-06 13:08:06 -0700

    Convert the Docker commands to Zsh

---
+++ config/zsh/docker.zsh
@@ -0,0 +1,23 @@
+# Ignore project names that aren't prepended with `tesla` or `edison`.
+# TODO: Genericize this.
+# TODO: Fix this function.
+function project_name {
+  string match -r '(tesla|edison)-(.+)' "$(basename "$(pwd)")" | sed -n 3p | sed 's/-/_/g'
+}
+
+alias k='kubectl'
+alias dc='docker-compose'
+alias dcr='docker-compose run --rm $(project_name)'
+alias dcu='docker-compose up -d'
+alias dcl='docker-compose logs -f $(project_name)'
+alias dcb='docker-compose build $(project_name)'
+
+# TODO: Genericize this.
+alias dco="$EDITOR ~/Development/tesla/docker-compose.override.yml"
+
+alias docker-kill='pgrep -i -f docker | xargs kill'
+
+function docker-nuke {
+  docker-compose down -v --rmi all --remove-orphans
+  docker system prune --all --force --volumes
+}
