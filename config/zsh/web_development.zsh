e4e0c943e3f4b4f0b30a6405462437b4892b6365
Landon Schropp <schroppl@gmail.com>
2019-08-06 14:59:10 -0700

    Move web development commands to zsh

--- config/zsh/web_development.zsh
+++ config/zsh/web_development.zsh
@@ -3,13 +3,5 @@
 # Starts a simple HTTP server in the current directory.
 alias server="python -m SimpleHTTPServer"
 
-# Get the external IP address. Taken from: http://alias.sh/get-external-ip.
-alias externalip='curl ifconfig.me'
-
 # Reload DNS on OS X. Taken from: http://alias.sh/reload-dns-osx.
 alias flushdns="dscacheutil -flushcache"
-
-# Open up a local server for testing on other devices.
-function serveo
-  ssh -R "80:localhost:$argv[1]" serveo.net
-end
