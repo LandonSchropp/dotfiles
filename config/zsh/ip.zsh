4d7ca079c9b5df9be76038eb8771a08c714bdeb6
Landon Schropp <schroppl@gmail.com>
2019-08-06 14:55:55 -0700

    Convert ip functions to Zsh

--- config/zsh/ip.zsh
+++ config/zsh/ip.zsh
@@ -1,12 +1,12 @@
 # Prints out the IP address for the current internal network connection.
-function internal_ip
+function internal-ip {
   ifconfig \
     | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' \
     | grep "inet " \
     | awk '{print $2}'
-end
+}
 
 # Prints out the external IP address of the current internet connection.
-function external_ip
+function external-ip {
   curl -s checkip.dyndns.org | sed -e "s/.*Current IP Address: //" -e "s/<.*\$//"
-end
+}
