770f1c39d9fa10575359ca72d77aa323fded623f
Landon Schropp <schroppl@gmail.com>
2020-02-04 11:19:47 -0800

    Fix a typo in a docker.zsh alias

--- config/zsh/docker.zsh
+++ config/zsh/docker.zsh
@@ -2,7 +2,7 @@ alias k='kubectl'
 alias dc='docker-compose'
 alias dcud='docker-compose-current-service-up-dependent'
 alias dcn='docker-compose-current-service-name'
-alias dc-envrc='docker-compose-envrc > .envrc'
+alias dc-envrc='docker-compose-current-service-envrc > .envrc'
 alias dco="docker-compose-edit-override"
 
 function dcr() {
