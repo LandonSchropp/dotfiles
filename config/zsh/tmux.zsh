9471e1203c11030d77eb67208dd0cec0234a311b
Landon Schropp <schroppl@gmail.com>
2021-02-09 19:08:13 -0600

    Update Tmux functions
    * Rename all of the functions to use hyphens instead of underscores.
    * Rename all the functions to long names, and create aliases for them.
    * Create a separate tmux-create function.
    * Remove dead code.
    * Fix the "no session" bug.
    * Adjust the automatically create Neovim window so closing Neovim doesn't automatically close the
      window. (This has tripped me up many times.)

--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -1,12 +1,8 @@
 TMUX_PRESETS_CONFIGURATION=~/Google\ Drive/Configuration/tmux-presets.json
 
-# Alias common tmux functions.
-alias td='tmux detach'
-alias tl='active_tmux_sessions'
-
-# If the session is in the list of current tmux sessions, it is attached. Otherwise, a new session
-# is created and attached with the argument as its name.
-function ta {
+# Creates a tmux session if it doesn't already exist.
+function tmux-create {
+  SESSION_NAME="$1"
 
   # Ensure the name of a session was provided.
   if [ $# -ne 1 ]; then
@@ -14,31 +10,46 @@ function ta {
     return 1
   fi
 
+  # If the session already exists, then don't do anything.
+  # https://superuser.com/questions/1174750/tmux-has-session-search-is-prefix-matching
+  if tmux-list-active-sessions | grep -Fx "$1" >/dev/null; then
+    return
+  fi
+
+  # Grab the configuration.
+  CONFIGURATION=$(
+    jq \
+      --arg name "$SESSION_NAME" \
+      -r \
+      '.[] | select(.name == $name)' \
+      $TMUX_PRESETS_CONFIGURATION \
+  )
+
+  # Grab the directory of the session from the tmux presets if it exists. Replace any tildes in
+  # the string with $HOME.
+  DIRECTORY=$(echo "$CONFIGURATION" | jq -r '.directory' | gsed -e "s#^~#$HOME#")
+  SERVER=$(echo "$CONFIGURATION" | jq -r '.serverCommand')
+
+  # If the session is not in the presets, use the current working directory.
+  DIRECTORY=$(test -z "$DIRECTORY" && pwd || echo "$DIRECTORY")
+
+  # Create a new session using my preferred two-window layout.
+  tmux new-session -d -c "$DIRECTORY" -s "$SESSION_NAME"
+  tmux rename-window working
+  tmux new-window -d -c "$DIRECTORY" -n vim
+
+  # NOTE: This command is run using send instead of passing it into `new-window` because it
+  # allows the command to be quit without affecting the window.
+  tmux send -t vim nvim ENTER
+}
+
+# If the session is in the list of current tmux sessions, it is attached. Otherwise, a new session
+# is created and attached with the argument as its name.
+function tmux-create-and-attach {
   SESSION_NAME="$1"
 
   # Create the session if it doesn't already exist.
-  # https://superuser.com/questions/1174750/tmux-has-session-search-is-prefix-matching
-  if ! active_tmux_sessions | grep -Fx "$1" >/dev/null; then
-
-    # Grab the directory from the tmux presets if it exists. Replace any tildes in the string with
-    # $HOME.
-    DIRECTORY=$(
-      jq \
-        --arg name "$SESSION_NAME" \
-        -r \
-        '.[] | select(.name == $name) | .directory' \
-        $TMUX_PRESETS_CONFIGURATION \
-        | gsed -e "s#^~#$HOME#"
-    )
-
-    # If the directory is empty, then use the current working directory.
-    DIRECTORY=$(test -z "$DIRECTORY" && pwd || echo "$DIRECTORY")
-
-    # Create a new session using my preferred two-window layout.
-    tmux new-session -d -c "$DIRECTORY" -s "$SESSION_NAME"
-    tmux rename-window working
-    tmux new-window -d -c "$DIRECTORY" -n vim nvim
-  fi
+  tmux-create "$SESSION_NAME"
 
   # If a tmux session is already attached, switch to the new session. Otherwise, attach the new
   # session.
@@ -50,7 +61,7 @@ function ta {
 }
 
 # A wrapper for tmux kill-session
-function tk {
+function tmux-kill {
   if [ $# -ne 0 ]; then
     tmux kill-session -t $argv[1]
   else
@@ -59,19 +70,19 @@ function tk {
 }
 
 # Move a tmux window to another position
-function tm {
+function tmux-move {
 
   TARGET_WINDOW="$(echo $argv[1])"
 
-  if [ $TARGET_WINDOW -lt 1 ] || [ $TARGET_WINDOW -gt $(tmux_number_of_windows) ]; then
-    echo -e "The target window must be between 1 and "$(tmux_number_of_windows)"."
+  if [ $TARGET_WINDOW -lt 1 ] || [ $TARGET_WINDOW -gt $(tmux-number-of-windows) ]; then
+    echo -e "The target window must be between 1 and $(tmux-number-of-windows)."
     return 1
   fi
 
-  STARTING_WINDOW=$(tmux_current_window)
+  STARTING_WINDOW=$(tmux-current-window)
 
-  if [ $TARGET_WINDOW -lt $(tmux_current_window) ]; then
-    for i in $(seq $TARGET_WINDOW $(tmux_current_window)); do
+  if [ $TARGET_WINDOW -lt $(tmux-current-window) ]; then
+    for i in $(seq $TARGET_WINDOW $(tmux-current-window)); do
       tmux swap-window -s $STARTING_WINDOW -t $i
     done
   fi
@@ -86,34 +97,41 @@ function tm {
 }
 
 # Lists all of the preset Tmux sessions.
-function preset_tmux_sessions {
+function tmux-list-available-sessions {
   jq -r 'map(.name) | sort | .[]' "$TMUX_PRESETS_CONFIGURATION"
 }
 
 # Lists all of the active Tmux sessions.
-function active_tmux_sessions {
+function tmux-list-active-sessions {
   tmux ls -F "#{session_name}" 2>/dev/null | cut -d: -f1
 }
 
 # Lists all available sessions from Tmux and the Tmux presets.
-function tmux_sessions {
-  echo $(preset_tmux_sessions) $(active_tmux_sessions) | gsed -e 's/\s\+/\n/g' | sort | uniq
+function tmux-sessions {
+  echo $(tmux-list-available-sessions) $(tmux-list-active-sessions) | gsed -e 's/\s\+/\n/g' | sort | uniq
 }
 
-function tmux_current_window {
+# Returns the current window number.
+function tmux-current-window {
   tmux display-message -p '#I'
 }
 
-function tmux_number_of_windows {
+# Returns the number of windows in the current session.
+function tmux-number-of-windows {
   tmux display-message -p '#{session_windows}'
 }
 
-# complete --no-files --command ta --arguments '(tmux_sessions)'
-# complete --no-files --command tk --arguments '(active_tmux_sessions)'
-
 # Kill all tmux sessions.
-function tka {
-  for session in $(active_tmux_sessions); do
-    tk $session
+function tmux-kill-all {
+  for session in $(tmux-list-active-sessions); do
+    tmux-kill $session
   done
 }
+
+# Define aliases for the functions.
+alias ta="tmux-create-and-attach"
+alias td="tmux detach"
+alias tk="tmux-kill"
+alias tka="tmux-kill-all"
+alias tl="tmux-list-active-sessions"
+alias tm="tmux-move"
