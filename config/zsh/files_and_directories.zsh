2b9e4651161ad2fb88274db21dc51f891ec6d194
Landon Schropp <schroppl@gmail.com>
2021-03-21 22:22:50 -0500

    Add tree command that ignores .gitignore files

--- config/zsh/files_and_directories.zsh
+++ config/zsh/files_and_directories.zsh
@@ -2,9 +2,12 @@ function raw-tree {
    /usr/local/bin/tree $argv
 }
 
-# function tree {
-#   git check-ignore * | paste -s -d '|' - | xargs -I _ tree -I _ $argv
-# }
+# Since ripgrep ignores files in .gitignore by default, we can pipe its output into tree to exclude
+# gitignored files. This should also work fine in directories that aren't part of Git repositories.
+function tree {
+  rg --files | /usr/local/bin/tree --fromfile
+}
+
 
 function touch {
   mkdir -p "$(dirname "$1")"
