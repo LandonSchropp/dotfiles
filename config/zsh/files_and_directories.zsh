95691414a3186c31ff2eb20e88a807b80513ce15
Landon Schropp <schroppl@gmail.com>
2021-04-25 00:26:04 -0500

    Dump custom tree implementation
    While I don't want to show gitignored files, this version of tree
    doesn't allow you to pass in arguments.

--- config/zsh/files_and_directories.zsh
+++ config/zsh/files_and_directories.zsh
@@ -2,12 +2,6 @@ function raw-tree {
    /usr/local/bin/tree $argv
 }
 
-# Since ripgrep ignores files in .gitignore by default, we can pipe its output into tree to exclude
-# gitignored files. This should also work fine in directories that aren't part of Git repositories.
-function tree {
-  rg --files | /usr/local/bin/tree --fromfile
-}
-
 
 function touch {
   mkdir -p "$(dirname "$1")"
