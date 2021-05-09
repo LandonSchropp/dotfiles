7948f42d7af4b2432ead2c5ee7e997309a5ede63
Landon Schropp <schroppl@gmail.com>
2021-05-09 14:00:28 -0500

    Conver init.nvim → init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,15 @@
-print("Hello from init.lua")
+vim.cmd("source ~/.config/nvim/plugins.vim")
+vim.cmd("source ~/.config/nvim/editing.vim")
+vim.cmd("source ~/.config/nvim/keybindings.vim")
+vim.cmd("source ~/.config/nvim/syntax.vim")
+vim.cmd("source ~/.config/nvim/theme.vim")
+
+-- Plugins
+vim.cmd("source ~/.config/nvim/plugin/cursor_position.vim")
+vim.cmd("source ~/.config/nvim/plugin/strip_whitespace.vim")
+vim.cmd("source ~/.config/nvim/plugin/test.vim")
+vim.cmd("source ~/.config/nvim/plugin/rename_extension.vim")
+
+-- Enable reading project-specific .nvimrc files
+vim.api.nvim_set_option("exrc", true)
+vim.api.nvim_set_option("secure", true)
