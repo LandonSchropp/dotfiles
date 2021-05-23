45346b93121ccbcaf53a2de8cc9269b75a1158a3
Landon Schropp <schroppl@gmail.com>
2021-05-23 13:24:33 -0500

    Add a todo to utilities

--- config/nvim/lua/utilities.lua
+++ config/nvim/lua/utilities.lua
@@ -1,4 +1,6 @@
 -- TODO: These commands should be replaced by the API commands when they're added.
+-- TODO: The set_option variable isn't equivilent to VimL's set option. This should be updated to
+-- use the new API when it's added. (https://github.com/neovim/neovim/pull/13479)
 
 local set_option = vim.api.nvim_set_option
 local set_variable = vim.api.nvim_set_var
