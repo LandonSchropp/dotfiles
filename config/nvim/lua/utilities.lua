0199af46e15cd48bca9a763979099df76e1d647b
Landon Schropp <schroppl@gmail.com>
2021-07-10 20:01:08 -0500

    Update settings to use vim.opt

--- config/nvim/lua/utilities.lua
+++ config/nvim/lua/utilities.lua
@@ -1,12 +1,6 @@
--- TODO: These commands should be replaced by the API commands when they're added.
--- TODO: The set_option variable isn't equivilent to VimL's set option. This should be updated to
--- use the new API when it's added. (https://github.com/neovim/neovim/pull/13479)
-
-local set_option = vim.api.nvim_set_option
-local set_variable = vim.api.nvim_set_var
-
+-- TODO: This command should be replaced by the API commands when an API is added.
 local function auto_command(event, pattern, command)
   vim.cmd("autocmd " .. event .. " " .. pattern .. " " .. command)
 end
 
-return { set_option = set_option, auto_command = auto_command, set_variable = set_variable }
+return { auto_command = auto_command }
