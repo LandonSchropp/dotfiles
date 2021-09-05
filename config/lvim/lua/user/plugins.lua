2c14e2d21b50018560e8d1aa53500ba89a4c8eee
Landon Schropp <schroppl@gmail.com>
2021-09-05 14:09:05 -0500

    Update the plugins list
    This enables many of my essential plugins. I left the plugins I'm still
    not sure about commented out for now.

--- config/lvim/lua/user/plugins.lua
+++ config/lvim/lua/user/plugins.lua
@@ -1,11 +1,74 @@
-return {
+lvim.plugins = {
 
-  -- A nice color scheme.
-  {"folke/tokyonight.nvim"},
+
+
+  -- GENERAL
+
+  -- Sets the project root to the current Git repository by default.
+  {'airblade/vim-rooter'},
 
   -- Displays diagnostics, references, telescope results, quickfix and location lists.
   {"folke/trouble.nvim"},
 
-  -- Sets the project root to the current Git repository by default.
-  {'airblade/vim-rooter'}
+  -- Quickly run Unix commands.
+  {"tpope/vim-eunuch"},
+
+  -- Automatically resize open splits so the current is the biggest
+  -- Plug 'roman/golden-ratio'
+
+  -- Automatically create any non-existent directories before writing the buffer
+  {"pbrisbin/vim-mkdir"},
+
+  -- Enable support for the repeat command with other plugins
+  {"tpope/vim-repeat"},
+
+  -- Extra commands for surrounding text
+  {"tpope/vim-surround"},
+
+
+
+  -- TEXT EDITING
+
+  -- Auto-completes quotes, brackets, etc.
+  -- Plug 'Raimondi/delimitMate'
+  --
+  -- Automatically close HTML tags
+  {"alvan/vim-closetag"},
+
+  -- Provides new motions to target when running operators
+  {"wellle/targets.vim"},
+
+  -- Automatically add end in Ruby
+  -- Plug 'tpope/vim-endwise'
+
+  -- Wrap and unwrap arguments and curly braces.
+  {"AndrewRadev/splitjoin.vim"},
+
+  -- Easy text replacement.
+  {"LandonSchropp/vim-stamp"},
+
+  -- Easily convert from camel case to underscore and back again.
+  {"tpope/vim-abolish"},
+
+  -- Automatically import JavaScript modules.
+  -- Plug 'Galooshi/vim-import-js'
+
+  -- Undo tree
+  {"mbbill/undotree"},
+
+  -- Swapping arguments
+  {"machakann/vim-swap"},
+
+
+
+  -- SYNTAX HIGHLIGHTING
+
+  -- Better Git conflict highlighting.
+  -- {'rhysd/conflict-marker.vim'}
+
+  -- A nice color scheme.
+  {"folke/tokyonight.nvim"},
+
+  -- Highlight CSS colors
+  {"ap/vim-css-color"}
 }
