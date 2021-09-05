46d33d8ff3ceb07e82bdf76c39ad3641e16c5c8d
Landon Schropp <schroppl@gmail.com>
2021-09-05 14:59:12 -0500

    Import old syntax settings

---
+++ config/lvim/lua/user/syntax.lua
@@ -0,0 +1,35 @@
+-- Simple helper function that runs an auto command, excluding newlines.
+local function auto_command(command)
+  vim.cmd("autocmd " .. string.gsub(command, "\n", " "))
+end
+
+-- Ignore spell checking for CamelCased items.
+-- Borrowed from: https://github.com/teranex/dotvim/blob/master/vimrc#L486
+auto_command( [[
+  Syntax * syn match CamelCase
+    "\(\<\|_\)\%(\u\l*\)\{2,}\(\>\|_\)\|\<\%(\l\l*\)\%(\u\l*\)\{1,}\>" transparent
+    containedin=.*Comment.*,.*String.*,VimwikiLink contains=@NoSpell contained
+]])
+
+-- Ignore spell checking for URLs.
+auto_command([[
+  Syntax * syn match UrlNoSpell
+    '\w\+:\/\/[^[:space:]"\']\+' transparent
+    containedin=.*Comment.*,.*String.* contains=@NoSpell contained
+]])
+
+-- Ignore spell checking for apostrophes.
+-- TODO: Determine if this is only needed for the camel cased items above. If so, figure out how to
+-- integrate it with that rule.
+auto_command([=[
+  Syntax * syn match UrlNoSpell
+    '['']s[[:space:]]' transparent
+    containedin=.*Comment.*,.*String.* contains=@NoSpell contained
+]=])
+
+-- When the type of shell script is /bin/sh, assume a POSIX-compatible shell for syntax highlighting
+-- purposes.
+vim.api.nvim_set_var('is_posix', 1)
+
+-- Display extra whitespace characters
+vim.opt.listchars = "tab:»·,trail:·,nbsp:·"
