0398071f47b498c9f5ea23292ab707542c317381
Landon Schropp <schroppl@gmail.com>
2021-09-05 15:28:52 -0500

    Move all remaining settings in config.lua

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -3,36 +3,3 @@ require("user.keys")
 require("user.plugins")
 require("user.theme")
 require("user.syntax")
-
--- General
-lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
-
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
-
--- Install all of the maintained language parsers.
-lvim.builtin.treesitter.ensure_installed = "maintained"
-lvim.builtin.treesitter.highlight.enabled = true
-
--- set a formatter if you want to override the default lsp one (if it exists)
--- lvim.lang.python.formatters = {
---   {
---     exe = "black",
---   }
--- }
--- set an additional linter
--- lvim.lang.python.linters = {
---   {
---     exe = "flake8",
---   }
--- }
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
