902406833846f78a799d54c453198c2d59af024f
Landon Schropp <schroppl@gmail.com>
2021-09-06 15:04:38 -0500

    Fix linter and formatter config
    I was running into too many problems with LunarVim's linter and
    formatter configurations, especially with eslint_d. I had much better
    luck using the underlying null-ls configuration instead. This config
    also didn't require me to specify file types, which was a nice bonus.

--- config/lvim/lua/user/language.lua
+++ config/lvim/lua/user/language.lua
@@ -1,21 +1,26 @@
+local null_ls = require("null-ls")
+
 -- For a list of all available formatters and linters, look here:
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#available-sources
 
 -- Enable formatting.
 lvim.format_on_save = true
 
--- Enable the formatters.
-lvim.lang.elixir.formatters = { { exe = "mix" } }
-lvim.lang.javascript.formatters = { { exe = "eslint_d" } }
-lvim.lang.javascriptreact.formatters = { { exe = "eslint_d" } }
-lvim.lang.json.formatters = { { exe = "fixjson" } }
-lvim.lang.lua.formatters = { { exe = "stylua" } }
--- TODO: Enable this formatter.
--- lvim.lang.markdown.formatters = { { exe = "write_good" }, { exe = "markdownlint" } }
-lvim.lang.ruby.formatters = { { exe = "rubocop" } }
+local sources = {
 
--- Enable the linters.
-lvim.lang.javascript.linters = { { exe = "eslint_d" } }
-lvim.lang.javascriptreact.linters = { { exe = "eslint_d" } }
-lvim.lang.ruby.linters = { { exe = "rubocop" } }
-lvim.lang.sh.linters = { { exe = "shellcheck" } }
+  -- Formatters
+  null_ls.builtins.formatting.eslint_d,
+  null_ls.builtins.formatting.fixjson,
+  null_ls.builtins.formatting.mix,
+  null_ls.builtins.formatting.stylua,
+
+  -- Linters
+  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.diagnostics.rubocop,
+  null_ls.builtins.diagnostics.shellcheck,
+  null_ls.builtins.diagnostics.markdownlint,
+  null_ls.builtins.diagnostics.write_good,
+}
+
+-- Enable the formatters and linters.
+null_ls.register({ sources = sources })
