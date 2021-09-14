8dea126aa26566fcf8dc0383328bd94196ee5740
Landon Schropp <schroppl@gmail.com>
2021-09-14 15:42:47 -0500

    Configure linters

--- config/lvim/lua/user/language.lua
+++ config/lvim/lua/user/language.lua
@@ -1,30 +1,13 @@
 local null_ls = require("null-ls")
 
--- For a list of all available formatters and linters, look here:
--- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#available-sources
-
--- Enable formatting.
-lvim.format_on_save = true
-
-local sources = {
-
-  -- Formatters
-  null_ls.builtins.formatting.eslint_d,
-  null_ls.builtins.formatting.fixjson,
-  -- null_ls.builtins.formatting.mix,
-  null_ls.builtins.formatting.stylua,
-
-  -- Linters
-  null_ls.builtins.diagnostics.eslint_d,
-  null_ls.builtins.diagnostics.rubocop,
-  null_ls.builtins.diagnostics.shellcheck,
-  null_ls.builtins.diagnostics.markdownlint,
-  null_ls.builtins.diagnostics.write_good,
-}
-
--- BUG FIX: I'd strongly prefer for this to use the regular null_ls formatters. However, that doesn't
--- seem to work for some reason in Elixir files.
+-- Formatters
+lvim.lang.lua.formatters = { { exe = "stylua" } }
+lvim.lang.json.formatters = { { exe = "fixjson" } }
 lvim.lang.elixir.formatters = { { exe = "mix" } }
+lvim.lang.javascript.formatters = { { exe = "eslint_d" } }
+lvim.lang.javascriptreact.formatters = { { exe = "eslint_d" } }
 
--- Enable the formatters and linters.
-null_ls.register({ sources = sources })
+-- Linters
+lvim.lang.ruby.linters = { { exe = "rubocop" } }
+lvim.lang.sh.linters = { { exe = "rubocop" } }
+-- lvim.lang.markdown.linters = { { exe = "markdownlint" }, { exe = "write_good" } }
