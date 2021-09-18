d1866e5130094455f7090d02e818137bd44d2761
Landon Schropp <schroppl@gmail.com>
2021-09-17 23:14:00 -0500

    Change the linter and formatter configurations

--- config/lvim/lua/user/language.lua
+++ config/lvim/lua/user/language.lua
@@ -1,13 +1,14 @@
-local null_ls = require("null-ls")
+-- FIX: Remove this after this issue is fixed: https://github.com/LunarVim/LunarVim/issues/1554.
+lvim.lang.markdown = {}
 
 -- Formatters
-lvim.lang.lua.formatters = { { exe = "stylua" } }
-lvim.lang.json.formatters = { { exe = "fixjson" } }
 lvim.lang.elixir.formatters = { { exe = "mix" } }
 lvim.lang.javascript.formatters = { { exe = "eslint_d" } }
-lvim.lang.javascriptreact.formatters = { { exe = "eslint_d" } }
+lvim.lang.json.formatters = { { exe = "fixjson" } }
+lvim.lang.lua.formatters = { { exe = "stylua" } }
 
 -- Linters
+lvim.lang.javascript.linters = { { exe = "eslint_d" } }
 lvim.lang.ruby.linters = { { exe = "rubocop" } }
-lvim.lang.sh.linters = { { exe = "rubocop" } }
--- lvim.lang.markdown.linters = { { exe = "markdownlint" }, { exe = "write_good" } }
+lvim.lang.sh.linters = { { exe = "shellcheck" } }
+lvim.lang.markdown.linters = { { exe = "markdownlint" }, { exe = "write_good" } }
