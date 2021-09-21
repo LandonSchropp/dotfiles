67964f0ddadc4766a2d7619311daea76fd60fa84
Landon Schropp <schroppl@gmail.com>
2021-09-21 16:52:02 -0500

    Fix JSX linter

--- config/lvim/lua/user/language.lua
+++ config/lvim/lua/user/language.lua
@@ -4,11 +4,13 @@ lvim.lang.markdown = {}
 -- Formatters
 lvim.lang.elixir.formatters = { { exe = "mix" } }
 lvim.lang.javascript.formatters = { { exe = "eslint_d" } }
+lvim.lang.javascriptreact.formatters = { { exe = "eslint_d" } }
 lvim.lang.json.formatters = { { exe = "fixjson" } }
 lvim.lang.lua.formatters = { { exe = "stylua" } }
 
 -- Linters
 lvim.lang.javascript.linters = { { exe = "eslint_d" } }
+lvim.lang.javascriptreact.linters = { { exe = "eslint_d" } }
 lvim.lang.ruby.linters = { { exe = "rubocop" } }
 lvim.lang.sh.linters = { { exe = "shellcheck" } }
 lvim.lang.markdown.linters = { { exe = "markdownlint" }, { exe = "write_good" } }
