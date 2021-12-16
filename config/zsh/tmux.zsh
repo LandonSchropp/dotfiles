826243e4e411fcf9f42756eaf0db3277a1e24f8a
Landon Schropp <schroppl@gmail.com>
2021-12-16 17:04:02 -0600

    Switch back to tmuxinator

--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -1,24 +1,5 @@
 # shellcheck shell=bash
 
-TMUX_PRESETS_CONFIGURATION=~/Google\ Drive/Configuration/tmux-presets.json
-
-function _validate-tmux-presets-configuration {
-  NAME_LENGTH=$(jq "map(.name) | length" "$TMUX_PRESETS_CONFIGURATION")
-  UNIQUE_NAME_LENGTH=$(jq "map(.name) | unique | length" "$TMUX_PRESETS_CONFIGURATION")
-  DIRECTORY_LENGTH=$(jq "map(.directory) | length" "$TMUX_PRESETS_CONFIGURATION")
-  UNIQUE_DIRECTORY_LENGTH=$(jq "map(.directory) | unique | length" "$TMUX_PRESETS_CONFIGURATION")
-
-  if [ "$NAME_LENGTH" -ne "$UNIQUE_NAME_LENGTH" ]; then
-    echo "⛔️ The tmux presets configuration file contains duplicate names."
-    return 1
-  fi
-
-  if [ "$DIRECTORY_LENGTH" -ne "$UNIQUE_DIRECTORY_LENGTH" ]; then
-    echo "⛔️ The tmux presets configuration file contains duplicate directories."
-    return 1
-  fi
-}
-
 # Creates a tmux session if it doesn't already exist.
 function tmux-create {
   SESSION_NAME="$1"
@@ -29,37 +10,19 @@ function tmux-create {
     return 1
   fi
 
-  # Ensure the configuration file is valid.
-  _validate-tmux-presets-configuration || return 1
-
   # If the session already exists, then don't do anything.
   # https://superuser.com/questions/1174750/tmux-has-session-search-is-prefix-matching
-  if tmux-list-active-sessions | grep -Fx "$1" >/dev/null; then
+  if tmux-list-active-sessions | grep -Fx "$SESSION_NAME" >/dev/null; then
     return
   fi
 
-  # Grab the configuration.
-  CONFIGURATION=$(
-    jq \
-      --arg name "$SESSION_NAME" \
-      -r \
-      '.[] | select(.name == $name)' \
-      "$TMUX_PRESETS_CONFIGURATION"
-  )
-
-  # Grab the directory of the session from the tmux presets if it exists. Replace any tildes in
-  # the string with $HOME. If the session is not in the presets, use the current working directory.
-  DIRECTORY=$(echo "$CONFIGURATION" | jq -r '.directory' | gsed -e "s#^~#$HOME#")
-  DIRECTORY=$(test -z "$DIRECTORY" && pwd || echo "$DIRECTORY")
-
-  # Create a new session using my preferred two-window layout.
-  tmux new-session -d -c "$DIRECTORY" -s "$SESSION_NAME"
-  tmux rename-window working
-  tmux new-window -d -c "$DIRECTORY" -n vim
-
-  # NOTE: This command is run using send instead of passing it into `new-window` because it
-  # allows the command to be quit without affecting the window.
-  tmux send -t vim nvim ENTER
+  # If the session is available in tmuxinator, start it. Otherwise, fall back to the default
+  # tmuxinator configuration.
+  if tmuxinator-list-sessions | grep -Fx "$SESSION_NAME" >/dev/null; then
+    tmuxinator start "$SESSION_NAME"
+  else
+    tmuxinator start -n "$SESSION_NAME" default
+  fi
 }
 
 # If the session is in the list of current tmux sessions, it is attached. Otherwise, a new session
@@ -116,8 +79,8 @@ function tmux-move {
 }
 
 # Lists all of the preset Tmux sessions.
-function tmux-list-available-sessions {
-  jq -r 'map(.name) | sort | .[]' "$TMUX_PRESETS_CONFIGURATION"
+function tmuxinator-list-sessions {
+  tmuxinator list --newline | tail -n +2
 }
 
 # Lists all of the active Tmux sessions.
@@ -125,9 +88,9 @@ function tmux-list-active-sessions {
   tmux ls -F "#{session_name}" 2>/dev/null | cut -d: -f1
 }
 
-# Lists all available sessions from Tmux and the Tmux presets.
+# Lists all available sessions from Tmux and Tmuxinator.
 function tmux-sessions {
-  echo "$(tmux-list-available-sessions)" "$(tmux-list-active-sessions)" \
+  echo "$(tmuxinator-list-sessions)" "$(tmux-list-active-sessions)" \
     | gsed -e 's/\s\+/\n/g' | sort | uniq
 }
 
