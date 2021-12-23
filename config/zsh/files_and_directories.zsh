72daafa5cd939ca2160d05a6e446d1aa4864b492
Landon Schropp <schroppl@gmail.com>
2021-12-23 14:14:58 -0600

    Refactor local tree
    Instead of trying to take a more complicated approach with wrapping
    tree, this new function simply ignores common directories we don't want
    to search.

--- config/zsh/files_and_directories.zsh
+++ config/zsh/files_and_directories.zsh
@@ -1,22 +1,6 @@
-# This function is like the built-in tree function, but it ignores files in a local .gitignore file.
-# Unlike the built-in tree function, this function only takes one optional argument, which is a
-# specific file or directory to print out.
+# This wraps tree, but ignores common directories we don't want to search.
 function tree {
-
-  # Ensure the number of arguments is correct.
-  if [ $# -gt 1 ]; then
-    echo "Usage: tree [directory]"
-    return 1
-  fi
-
-  # If a directory was provided, navigate to that directory. This is done so the top-level
-  if [ $# -eq 1 ]; then
-    cd "$1"
-  fi
-
-  # Display the files with tree. Replace the first line with the current directory name so we can
-  # always tell what the directory is.
-  rg --files | /usr/local/bin/tree --fromfile | sed "1s/.*/$(basename $(pwd))/"
+  /usr/local/bin/tree -I 'node_modules|.git|build|dist' "$@"
 }
 
 # This function is just like the built-in though function, but it also automatically makes the
