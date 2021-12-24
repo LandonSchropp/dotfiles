1dea7eef5373b7168ff72e64b2c2603dd20b8e37
Landon Schropp <schroppl@gmail.com>
2021-12-24 12:43:46 -0600

    Clean up Lualine config

--- config/lvim/lua/landon/lualine.lua
+++ config/lvim/lua/landon/lualine.lua
@@ -3,12 +3,15 @@ local components = require("lvim.core.lualine.components")
 -- Configure the status line.
 -- TODO: Enable the curved separator characters once the following bug is fixed:
 -- https://github.com/ryanoasis/nerd-fonts/issues/661
--- lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
-lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+-- lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
 lvim.builtin.lualine.options.component_separators = "|"
 
 local function location()
-  return vim.fn.line(".") .. "/" .. vim.fn.line("$")
+  line = vim.fn.line(".")
+  number_of_lines = vim.fn.line("$")
+  line = string.format("%0" .. string.len(tostring(number_of_lines)) .. "d", line)
+  return " " .. line .. "/" .. number_of_lines
 end
 
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
