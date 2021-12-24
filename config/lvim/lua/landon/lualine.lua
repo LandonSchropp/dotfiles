2b1ed52ec1004175b77ea08afa537c8ce1722106
Landon Schropp <schroppl@gmail.com>
2021-12-23 18:34:32 -0600

    Clean up the Lualine settings

--- config/lvim/lua/landon/lualine.lua
+++ config/lvim/lua/landon/lualine.lua
@@ -3,12 +3,9 @@ local components = require("lvim.core.lualine.components")
 -- Configure the status line.
 -- TODO: Enable the curved separator characters once the following bug is fixed:
 -- https://github.com/ryanoasis/nerd-fonts/issues/661
-lvim.builtin.lualine.options.icons_enabled = true
-lvim.builtin.lualine.options.section_separators = { "", "" }
-lvim.builtin.lualine.options.section_separators = { "a", "b" }
-lvim.builtin.lualine.options.component_separators = { "", "" }
--- lvim.builtin.lualine.options.section_separators = {"", ""}
--- lvim.builtin.lualine.options.component_separators = {"", ""}
+-- lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+lvim.builtin.lualine.options.component_separators = "|"
 
 local function location()
   return vim.fn.line(".") .. "/" .. vim.fn.line("$")
@@ -20,7 +17,6 @@ lvim.builtin.lualine.sections.lualine_c = {
   { "filename", color = {}, cond = nil, left = 0, right = 0 },
   { "filetype", left = 0, right = 0 },
 }
-
 
 -- TODO: For some reason, this causes the LunarVim status page to load with line numbers 50% of the
 -- time when the separator option is used.
