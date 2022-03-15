edcdfd5cb4667c2f251388401a775ea76bee18e9
Landon Schropp <schroppl@gmail.com>
2022-03-15 11:23:24 -0500

    Move path functions to zprofile

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -1,6 +1,3 @@
-# Start up rbenv
-eval "$(rbenv init -)"
-
 # Start up pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
@@ -15,19 +12,11 @@ export NVM_DIR="$HOME/.nvm"
 source "$(brew --prefix nvm)/nvm.sh"
 source "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
 
-# Add the local bin paths to the system path. This isn't included in the .zshenv file because it
-# *must* be done after rbenv and nvm have loaded. Since we likely don't want relative paths in
-# non-shell instances of zsh anyway, this is probably better anyway.
-export PATH="./bin:./node_modules/.bin:$PATH"
-
 # Add the Google Cloud utilities to the path.
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+# source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+# source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 
 # Enable Homebrew auto completions.
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 fi
-
-# Add LunarVim to the path.
-export PATH="$HOME/.local/bin:$PATH"
---
+++ zprofile
@@ -0,0 +1,18 @@
+# Add Homebrew to the PATH.
+eval $(/opt/homebrew/bin/brew shellenv)
+
+# Add Pyenv to the PATH.
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+# Start up rbenv
+eval "$(rbenv init -)"
+
+# Add the local bin paths to the system path. This isn't included in the .zshenv file because it
+# *must* be done after rbenv and nvm have loaded. Since we likely don't want relative paths in
+# non-shell instances of zsh anyway, this is probably better anyway.
+export PATH="./bin:./node_modules/.bin:$PATH"
+
+# Add LunarVim to the path.
+export PATH="$HOME/.local/bin:$PATH"
