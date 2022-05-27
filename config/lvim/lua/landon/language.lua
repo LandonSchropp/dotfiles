230ddabb2c28b7cb2ffd09145122bc5d8e799d49
Landon Schropp <schroppl@gmail.com>
2022-05-27 14:00:25 -0500

    Fix a variable shadowing warning

--- config/lvim/lua/landon/language.lua
+++ config/lvim/lua/landon/language.lua
@@ -58,9 +58,9 @@ lvim.lsp.on_attach_callback = function(client, buffer_number)
   ts_utils.setup_client(client)
 
   -- HACK: TypeScript diagnostics are required in order to enable ts-util's import functions.
-  local filetype = vim.api.nvim_buf_get_option(buffer_number, "filetype")
+  local buffer_filetype = vim.api.nvim_buf_get_option(buffer_number, "filetype")
 
-  if filetype == "javascript" or filetype == "javascriptreact" then
+  if buffer_filetype == "javascript" or buffer_filetype == "javascriptreact" then
     vim.diagnostic.disable(buffer_number, vim.lsp.diagnostic.get_namespace(client.id))
   end
 end
