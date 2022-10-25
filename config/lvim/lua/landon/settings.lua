262b6e1930ba5b436168d68be967d27955cd0a9f
Landon Schropp <schroppl@gmail.com>
2022-10-25 00:47:00 -0500

    Change the path display

--- config/lvim/lua/landon/settings.lua
+++ config/lvim/lua/landon/settings.lua
@@ -187,7 +187,7 @@ lvim.builtin.telescope.pickers = { find_files = { hidden = true } }
 
 -- Improve the Telescope display.
 lvim.builtin.telescope.defaults.layout_config.width = 0.999999
-lvim.builtin.telescope.defaults.path_display = { "smart" }
+lvim.builtin.telescope.defaults.path_display = { "truncate" }
 
 -- Improve the Telescope sorting algorithm.
 telescope.load_extension("fzf")
