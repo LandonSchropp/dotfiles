b936397ae0e8286c7998adee0b32ea25d7fe4700
Landon Schropp <schroppl@gmail.com>
2023-01-01 22:06:17 -0600

    Move hack checktime command

--- config/lvim/lua/landon/settings.lua
+++ config/lvim/lua/landon/settings.lua
@@ -1,12 +1,3 @@
--- Automatically run checktime whenever the Neovim window gains focus. This should make the autoread
--- behavior work as expected. (https://github.com/neovim/neovim/issues/1380)
-vim.api.nvim_create_autocmd("FocusGained", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_command("checktime")
-  end,
-})
-
 -- Customize the dashboard buttons.
 lvim.builtin.alpha.dashboard.section.buttons = {
   entries = {
--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -1,2 +1,12 @@
 require ("custom.settings")
 require ("custom.commands")
+
+-- HACK: Automatically run checktime whenever the Neovim window gains focus. This should make the
+-- autoread behavior work as expected. (https://github.com/neovim/neovim/issues/1380)
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_command("checktime")
+  end,
+})
+
