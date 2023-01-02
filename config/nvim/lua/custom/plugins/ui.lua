fdd11c2fc870d02b218adc1bdc7c12a5710c7c26
Landon Schropp <schroppl@gmail.com>
2023-01-01 18:42:08 -0600

    Clean up the status line

--- config/nvim/lua/custom/plugins/ui.lua
+++ config/nvim/lua/custom/plugins/ui.lua
@@ -2,6 +2,9 @@ return {
   override_options = {
     tabufline = {
       enabled = false
-    }
+    },
+    statusline = {
+      separator_style = "arrow", -- default/round/block/arrow
+   },
   }
 }
