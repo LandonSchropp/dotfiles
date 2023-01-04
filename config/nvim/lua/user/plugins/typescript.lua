220576c76586c5f7199610c2a3ed27671aff58df
Landon Schropp <schroppl@gmail.com>
2023-01-04 00:51:17 -0600

    Improve the TypeScript server configuration

--- config/nvim/lua/user/plugins/typescript.lua
+++ config/nvim/lua/user/plugins/typescript.lua
@@ -1,1 +1,5 @@
-require("typescript").setup({})
+require("typescript").setup({
+  -- Configure the TypeScript server according to the AstroNvim instructions.
+  -- https://astronvim.github.io/Recipes/advanced_lsp#typescript-typescriptnvim
+  server = astronvim.lsp.server_settings("tsserver"),
+})
