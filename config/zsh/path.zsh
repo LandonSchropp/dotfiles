a858da61d389dbeb3841c5157850835bddab1db6
Landon Schropp <schroppl@gmail.com>
2023-01-07 21:54:38 -0600

    Remove Google Cloud SDK

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -12,10 +12,6 @@ export NVM_DIR="$HOME/.nvm"
 source "$(brew --prefix nvm)/nvm.sh"
 source "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
 
-# Add the Google Cloud utilities to the path.
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-
 # Enable Homebrew auto completions.
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
