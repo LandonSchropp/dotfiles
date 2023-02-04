1b25a3da5701d4b92346c47f337d0ce74f94d29a
Landon Schropp <schroppl@gmail.com>
2023-02-04 14:33:40 -0600

    Disable notifications UI

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -3,6 +3,13 @@ return {
   opts = {
     presets = {
       inc_rename = true,
+      lsp_doc_border = true,
+    },
+    messages = {
+      enabled = false,
+    },
+    notify = {
+      enabled = false,
     },
   },
 }
