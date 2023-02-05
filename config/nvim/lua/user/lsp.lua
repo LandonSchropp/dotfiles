799d0655b1adacdc6749037bb8c870738632bfa9
Landon Schropp <schroppl@gmail.com>
2023-02-05 17:26:22 -0600

    Remove unneeded AstroNvim config
    This config is already included in LazyVim.

--- config/nvim/lua/user/lsp.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- Skip the tsserver setup so typescript.nvim can do it instead (per the AstroNvim instructions).
-  -- https://astronvim.github.io/Recipes/advanced_lsp#typescript-typescriptnvim
-  skip_setup = { "tsserver" },
-}
