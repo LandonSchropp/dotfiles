79f574bb99238a47aaa646d7169a200ab5f5144b
Landon Schropp <schroppl@gmail.com>
2023-02-05 19:57:52 -0600

    Remove unused groups from which-key

---
+++ config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/which-key.nvim",
+
+  -- Override the config function to remove/replace the groups.
+  config = function(_, options)
+    local which_key = require("which-key")
+
+    which_key.setup(options)
+
+    which_key.register({
+      mode = { "n", "v" },
+      ["g"] = { name = "+Goto" },
+      ["]"] = { name = "+Next" },
+      ["["] = { name = "+Prevous" },
+      ["<leader>b"] = { name = "+Buffer" },
+      ["<leader>l"] = { name = "+Language/LSP" },
+      ["<leader>f"] = { name = "+Find" },
+      ["<leader>g"] = { name = "+Git" },
+      ["<leader>gh"] = { name = "+Hunks" },
+    })
+  end,
+}
