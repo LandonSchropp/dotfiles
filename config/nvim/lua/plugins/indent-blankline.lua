f73df462ab6b4ae98d1a890fbfee4e87bafac987
Landon Schropp <schroppl@gmail.com>
2023-02-05 20:07:47 -0600

    Improve the indent-blankline.nvim config

---
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    show_current_context = true,
+    char = "▏",
+    context_char = "▏",
+    show_first_indent_level = true,
+    show_current_context_start = false,
+  },
+}
