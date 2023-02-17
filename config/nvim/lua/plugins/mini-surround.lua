6b1477c97536c3caf66bc424d3b6be4751d5d36e
Landon Schropp <schroppl@gmail.com>
2023-02-17 15:33:07 -0600

    Fix error in mini.surround config
    I've opened up a bug ticket in mini.nvim to address this.

--- config/nvim/lua/plugins/mini-surround.lua
+++ config/nvim/lua/plugins/mini-surround.lua
@@ -8,8 +8,9 @@ return {
       find_left = "yF",
       highlight = "vs",
       replace = "cs",
-      update_n_lines = "",
+      -- update_n_lines = "",
     },
+    search_method = "cover_or_next",
   },
   config = function(_, opts)
     require("mini.surround").setup(opts)
