4c96e3e54ce7838eef6733b0eed078f78e1da444
Landon Schropp <schroppl@gmail.com>
2023-03-01 19:32:01 -0700

    Open Spectre in insert mode

--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -3,8 +3,12 @@ local keymap_utils = require("util.keymaps")
 return {
   "windwp/nvim-spectre",
   keys = function(_, keymaps)
-    keymap_utils.remap(keymaps, "<leader>sr", "<leader>fr")
-    keymap_utils.find_required(keymaps, "<leader>fr").desc = "Find and replace"
+    keymap_utils.remove(keymaps, "<leader>sr")
+
+    -- Add a custom open function that opens Spectre in insert mode.
+    keymap_utils.add(keymaps, "<leader>fr", function()
+      require("spectre").open({ is_insert_mode = true })
+    end, { desc = "Find and replace" })
 
     return keymaps
   end,
