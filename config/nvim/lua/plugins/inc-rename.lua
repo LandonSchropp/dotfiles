6ff339442b0dd7801a02426efbe702276c8d5d3a
Landon Schropp <schroppl@gmail.com>
2023-03-02 12:32:05 -0700

    Enable empty inc renames

--- config/nvim/lua/plugins/inc-rename.lua
+++ config/nvim/lua/plugins/inc-rename.lua
@@ -1,6 +1,8 @@
 return {
   "smjonas/inc-rename.nvim",
   config = function()
-    require("inc_rename").setup()
+    require("inc_rename").setup({
+      preview_empty_name = true,
+    })
   end,
 }
