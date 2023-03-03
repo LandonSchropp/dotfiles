471e04efcde4c9b7b858f6ddefbe347e21231b92
Landon Schropp <schroppl@gmail.com>
2023-03-03 14:38:26 -0800

    Change the Neo-tree keymapping

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -4,11 +4,11 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   keys = function(_, keymaps)
     keymap_utils.remove(keymaps, "<leader>fE")
-    keymap_utils.remove(keymaps, "<leader>e")
+    keymap_utils.remove(keymaps, "<leader>fe")
     keymap_utils.remove(keymaps, "<leader>E")
 
     -- Modify the description of the existing explorer keymap.
-    keymap_utils.find_required(keymaps, "<leader>fe").desc = "Neotree explorer"
+    keymap_utils.find_required(keymaps, "<leader>e").desc = "File explorer"
 
     return keymaps
   end,
