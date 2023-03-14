ccfb514f0304ac4819ea0c9c2ac7475d99f09b45
Landon Schropp <schroppl@gmail.com>
2023-03-13 22:50:42 -0700

    Tweak Luasnip shortcuts

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -1,3 +1,7 @@
+local load_snippets = function()
+  require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+end
+
 return {
   "L3MON4D3/LuaSnip",
   -- These options were taken from the recommendations by TJ DeVries.
@@ -7,7 +11,24 @@ return {
     update_events = { "TextChanged", "TextChangedI" },
     enable_autosnippets = true,
   },
-  config = function()
-    require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
-  end,
+  config = load_snippets,
+  keys = {
+    {
+      "<c-n>",
+      function()
+        local luasnip = require("luasnip")
+
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end,
+      mode = { "i", "s" },
+      desc = "Swap Luasnip choice",
+    },
+    {
+      "<leader>ls",
+      load_snippets,
+      desc = "Reload snippets",
+    },
+  },
 }
