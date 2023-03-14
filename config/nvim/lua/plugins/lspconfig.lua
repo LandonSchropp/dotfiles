5b22af1c6d1ec1d0da23c7ee86eaf31ecaa62657
Landon Schropp <schroppl@gmail.com>
2023-03-13 22:50:06 -0700

    Fix diagnostics keymap

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ return {
     local keymap_utils = require("util.keymaps")
     local keymaps = require("lazyvim.plugins.lsp.keymaps").get()
 
-    keymap_utils.remap(keymaps, "<leader>cd", "<leader>li")
+    keymap_utils.remap(keymaps, "<leader>cd", "gd")
     keymap_utils.remap(keymaps, "]d", "]]")
     keymap_utils.remap(keymaps, "[d", "[[")
     keymap_utils.remap(keymaps, "<leader>ca", "<leader>la")
