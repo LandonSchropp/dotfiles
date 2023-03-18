2311a2bca3e0cd6892e2ca49f8d445fc739c1db7
Landon Schropp <schroppl@gmail.com>
2023-03-18 00:48:54 -0700

    Supercharge ruby-script snippet

--- config/nvim/lua/snippets/all.lua
+++ config/nvim/lua/snippets/all.lua
@@ -6,18 +6,22 @@ local snippet_utils = require("util.snippet")
 
 local constantize = snippet_utils.constantize
 
+local dynamic = luasnip.d
+local insert = luasnip.i
 local snippet = luasnip.s
 local snippet_node = luasnip.sn
-local insert = luasnip.i
-local dynamic = luasnip.d
+local text = luasnip.t
+
+local BASH_SHEBANG = "#!/usr/bin/env bash"
+local RUBY_SHEBANG = "#!/usr/bin/env ruby"
 
 return {
-  parse_snippet("bash-shebang", "#!/usr/bin/env bash\n\n$0"),
+  parse_snippet("bash-shebang", BASH_SHEBANG .. "\n\n$0"),
   snippet(
     "bash-script",
     format(
       [[
-        #!/usr/bin/env bash
+        <>
 
         set -euo pipefail
 
@@ -26,6 +30,7 @@ return {
         <>
       ]],
       {
+        text(BASH_SHEBANG),
         insert(1, "1"),
         dynamic(2, function(args)
           local number_of_arguments = tonumber(args[1][1]) or 0
@@ -36,7 +41,7 @@ return {
 
           for index = 1, number_of_arguments do
             usage_template = usage_template .. " <<<>>>"
-            arguments_template = arguments_template .. ('<>="$%d"\n'):format(index, index)
+            arguments_template = arguments_template .. ('<>="$%d"\n'):format(index)
 
             table.insert(nodes, index, insert(index, "argument" .. tostring(index)))
             table.insert(nodes, constantize(index))
@@ -54,18 +59,47 @@ return {
       }
     )
   ),
-  parse_snippet("ruby-shebang", "#!/usr/bin/env ruby\n\n$0"),
-  parse_snippet(
+  parse_snippet("ruby-shebang", RUBY_SHEBANG .. "\n\n$0"),
+  snippet(
     "ruby-script",
-    [[
-      #!/usr/bin/env ruby
+    format(
+      [[
+        <>
 
-      unless ARGV.length == $1
-        STDERR.puts "Usage: #{\$0} $2"
-        exit 1
-      fi
-      
-      $0
-    ]]
+        # frozen_string_literal: true
+
+        unless ARGV.length == <>
+        <>
+        <>
+      ]],
+      {
+        text(RUBY_SHEBANG),
+        insert(1, "1"),
+        dynamic(2, function(args)
+          local number_of_arguments = tonumber(args[1][1]) or 0
+
+          local usage_template = ""
+          local arguments_template = number_of_arguments == 0 and "" or "\n"
+          local nodes = {}
+
+          for index = 1, number_of_arguments do
+            usage_template = usage_template .. " <<<>>>"
+            arguments_template = arguments_template .. ("<> = ARGV[%d]\n"):format(index - 1)
+
+            table.insert(nodes, index, insert(index, "argument" .. tostring(index)))
+            table.insert(nodes, constantize(index))
+          end
+
+          local template = '  warn "Usage: #{$PROGRAM_NAME}'
+            .. usage_template
+            .. '"\n  exit 1\nend\n'
+            .. arguments_template
+            .. "\n"
+
+          return snippet_node(nil, format(template, nodes, { dedent = false }))
+        end, { 1 }),
+        insert(0, "# TODO: Implement the script!"),
+      }
+    )
   ),
 }
