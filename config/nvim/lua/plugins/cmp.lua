15cb976e645fd6bacb6553483b7083b5f088dad5
Landon Schropp <schroppl@gmail.com>
2023-03-17 23:26:01 -0700

    Remove custom cmp mapping

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -36,14 +36,6 @@ return {
       end
     end, { "i", "s" })
 
-    options.mapping["<esc>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.close()
-      else
-        fallback()
-      end
-    end, { "i", "s" })
-
     return options
   end,
 }
