1c71a7e987aff800218aa6da60f15740d8268f37
Landon Schropp <schroppl@gmail.com>
2023-08-14 23:29:38 -0700

    Fix Genghis config
    The previous config would break if the plugin had not yet been
    installed.

--- config/nvim/lua/user/plugins/genghis.lua
+++ config/nvim/lua/user/plugins/genghis.lua
@@ -1,16 +1,36 @@
+local function copy_path()
+  require("genghis").copyFilepath()
+end
+
+local function copy_filename()
+  require("genghis").copyFilename()
+end
+
+local function chmodx()
+  require("genghis").chmodx()
+end
+
+local function rename_file()
+  require("genghis").renameFile()
+end
+
+local function move()
+  require("genghis").moveAndRenameFile()
+end
+
+local function delete()
+  require("genghis").trashFile()
+end
+
 return {
   "chrisgrieser/nvim-genghis",
   dependencies = "stevearc/dressing.nvim",
-  keys = function()
-    local genghis = require("genghis")
-
-    return {
-      { "<leader>Fy", genghis.copyFilepath,      desc = "Copy path" },
-      { "<leader>FY", genghis.copyFilename,      desc = "Copy name" },
-      { "<leader>Fx", genghis.chmodx,            desc = "chmod +x" },
-      { "<leader>Fr", genghis.renameFile,        desc = "Rename" },
-      { "<leader>Fm", genghis.moveAndRenameFile, desc = "Move and rename" },
-      { "<leader>Fd", genghis.trashFile,         desc = "Delete" },
-    }
-  end,
+  keys = {
+    { "<leader>Fy", copy_path,     desc = "Copy path" },
+    { "<leader>FY", copy_filename, desc = "Copy name" },
+    { "<leader>Fx", chmodx,        desc = "chmod +x" },
+    { "<leader>Fr", rename_file,   desc = "Rename" },
+    { "<leader>Fm", move,          desc = "Move" },
+    { "<leader>Fd", delete,        desc = "Delete" },
+  },
 }
