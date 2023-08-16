10c49f399654031a79ba963f4808522546ca2075
Landon Schropp <schroppl@gmail.com>
2023-08-15 23:04:57 -0700

    Add gitconflict.nvim

---
+++ config/nvim/lua/user/plugins/git-conflict.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  opts = {
+    default_mappings = {
+      ours = "go",
+      theirs = "gt",
+      none = "g0",
+      both = "gb",
+    },
+  },
+  keys = {
+    { "<leader>gq", "<cmd>GitConflictListQf<cr>", desc = "Git conflict list" },
+  },
+}
