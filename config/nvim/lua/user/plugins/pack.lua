0620c77dcf459f56c1deccc1e2c3c44d14a7c452
Landon Schropp <schroppl@gmail.com>
2023-08-15 22:44:10 -0700

    Add AstroNvim Community language packages
    These save a ton of time and effort!

---
+++ config/nvim/lua/user/plugins/pack.lua
@@ -0,0 +1,16 @@
+return {
+  { "AstroNvim/astrocommunity" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+}
