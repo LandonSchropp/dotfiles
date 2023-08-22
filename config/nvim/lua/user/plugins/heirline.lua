35a645247158644e14667a31c543569d93179f97
Landon Schropp <schroppl@gmail.com>
2023-08-22 11:01:10 -0700

    Disable Heirline

---
+++ config/nvim/lua/user/plugins/heirline.lua
@@ -0,0 +1,7 @@
+-- Heirline is way more trouble than it's worth. Because everything is super custom, there's almost
+-- no configuration options available. Instead, I'm opting to use purpose-built plugins for this
+-- functionality instead.
+return {
+  "rebelot/heirline.nvim",
+  enabled = false,
+}
