2012b64c4b0b38bd905c9c5278a66c60986c023e
Landon Schropp <schroppl@gmail.com>
2023-11-08 18:38:30 -0800

    Dropp mason-null-ls and explicitly load linters

--- config/nvim/lua/user/plugins/mason-null-ls.lua
+++ config/nvim/lua/user/plugins/mason-null-ls.lua
@@ -1,22 +1,6 @@
+-- This plugin has been causing me headaches in how it configures the language servers. I only use a
+-- few language servers, so the rest seem redundant, and I don't usually want them to auto start.
 return {
   "jay-babu/mason-null-ls.nvim",
-  opts = function(_, opts)
-    local has_rubocop = function(util)
-      return util.root_has_file(".rubocop.yml")
-    end
-
-    opts.handlers.rubocop = function()
-      local null_ls = require("null-ls")
-
-      local options = {
-        condition = has_rubocop,
-        command = "bundle",
-        extra_args = { "exec", "rubocop", "--server" },
-        prepend_extra_args = true,
-      }
-
-      null_ls.register(null_ls.builtins.diagnostics.rubocop.with(options))
-      null_ls.register(null_ls.builtins.formatting.rubocop.with(options))
-    end
-  end,
+  enabled = false,
 }
---
+++ config/nvim/lua/user/plugins/null-ls.lua
@@ -0,0 +1,59 @@
+local null_ls = require("null-ls")
+
+local has_rubocop = function(util)
+  return util.root_has_file(".rubocop.yml")
+end
+
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, opts)
+    local rubocop_options = {
+      condition = has_rubocop,
+      command = "bundle",
+      extra_args = { "exec", "rubocop", "--server" },
+      prepend_extra_args = true,
+    }
+
+    opts.sources = {
+
+      -- Git
+      null_ls.builtins.code_actions.gitrebase,
+      null_ls.builtins.code_actions.gitsigns,
+
+      -- JavaScript/TypeScript
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.formatting.eslint_d,
+
+      -- JSON
+      null_ls.builtins.formatting.fixjson,
+
+      -- Lua
+      null_ls.builtins.formatting.stylua,
+
+      -- Elixir
+      null_ls.builtins.formatting.mix,
+
+      -- Shell
+      null_ls.builtins.code_actions.shellcheck,
+      null_ls.builtins.diagnostics.shellcheck,
+      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.diagnostics.zsh,
+
+      -- Docker
+      null_ls.builtins.diagnostics.hadolint,
+
+      -- HTML/XML
+      null_ls.builtins.diagnostics.tidy,
+
+      -- Just
+      null_ls.builtins.formatting.just,
+
+      -- Ruby
+      null_ls.builtins.diagnostics.rubocop.with(rubocop_options),
+      null_ls.builtins.formatting.rubocop.with(rubocop_options),
+    }
+
+    return opts
+  end,
+}
