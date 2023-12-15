5a22d6f860e63c42b232018eeda09da32d6dc1d3
Landon Schropp <schroppl@gmail.com>
2023-12-15 13:48:25 -0800

    Add shortcut to toggle explorer

--- config/nvim/lua/user/mappings/ui.lua
+++ config/nvim/lua/user/mappings/ui.lua
@@ -9,9 +9,9 @@ return function(mappings)
   -- Remove UI mappings
   for key, _ in pairs(mappings.n) do
     if
-      key ~= UI_PREFIX
-      and starts_with(key, UI_PREFIX)
-      and not vim.tbl_contains(MAPPINGS_TO_KEEP, key)
+        key ~= UI_PREFIX
+        and starts_with(key, UI_PREFIX)
+        and not vim.tbl_contains(MAPPINGS_TO_KEEP, key)
     then
       updated_mappings.n[key] = false
     end
@@ -19,6 +19,7 @@ return function(mappings)
 
   -- Add mappings
   updated_mappings.n["<leader>uc"] = { ":Copilot toggle<cr>", desc = "Toggle Copilot" }
+  updated_mappings.n["<leader>ue"] = { ":Neotree toggle<cr>", desc = "Toggle Explorer" }
 
   return updated_mappings
 end
