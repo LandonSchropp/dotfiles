5ee181b9a434b0795aa808b9f75f4085f8560b1d
Landon Schropp <schroppl@gmail.com>
2023-12-26 15:18:40 -0600

    Configure cssls to ignore unknown at rules

---
+++ config/nvim/lua/user/lsp/config/cssls.lua
@@ -0,0 +1,22 @@
+return {
+  settings = {
+    css = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    scss = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    less = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+}
