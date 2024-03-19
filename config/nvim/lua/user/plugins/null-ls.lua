fb396d459b6e2b8f02a19b9617c5d3b7cd0b0c1c
Landon Schropp <schroppl@gmail.com>
2024-03-19 10:15:34 -0700

    Set up Ruby LSP
    😍

--- config/nvim/lua/user/plugins/null-ls.lua
+++ config/nvim/lua/user/plugins/null-ls.lua
@@ -6,29 +6,22 @@ end
 
 local has_prettier = function(util)
   return util.root_has_file(".prettierrc")
-    or util.root_has_file(".prettierrc.json")
-    or util.root_has_file(".prettierrc.yml")
-    or util.root_has_file(".prettierrc.yaml")
-    or util.root_has_file(".prettierrc.json5")
-    or util.root_has_file(".prettierrc.js")
-    or util.root_has_file(".prettierrc.cjs")
-    or util.root_has_file("prettier.config.js")
-    or util.root_has_file(".prettierrc.mjs")
-    or util.root_has_file("prettier.config.mjs")
-    or util.root_has_file("prettier.config.cjs")
-    or util.root_has_file(".prettierrc.toml")
+      or util.root_has_file(".prettierrc.json")
+      or util.root_has_file(".prettierrc.yml")
+      or util.root_has_file(".prettierrc.yaml")
+      or util.root_has_file(".prettierrc.json5")
+      or util.root_has_file(".prettierrc.js")
+      or util.root_has_file(".prettierrc.cjs")
+      or util.root_has_file("prettier.config.js")
+      or util.root_has_file(".prettierrc.mjs")
+      or util.root_has_file("prettier.config.mjs")
+      or util.root_has_file("prettier.config.cjs")
+      or util.root_has_file(".prettierrc.toml")
 end
 
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, opts)
-    local rubocop_options = {
-      condition = has_rubocop,
-      command = "bundle",
-      extra_args = { "exec", "rubocop", "--server" },
-      prepend_extra_args = true,
-    }
-
     opts.sources = {
 
       -- Git
@@ -55,10 +48,6 @@ return {
 
       -- Just
       null_ls.builtins.formatting.just,
-
-      -- Ruby
-      null_ls.builtins.diagnostics.rubocop.with(rubocop_options),
-      null_ls.builtins.formatting.rubocop.with(rubocop_options),
     }
 
     return opts
