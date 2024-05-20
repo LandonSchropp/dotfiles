6437306cec6b893736628e42882a6eacccfae81f
Landon Schropp <schroppl@gmail.com>
2024-05-19 23:19:24 -0500

    Configure astrocore

--- config/nvim/lua/plugins/astrocore.lua
+++ config/nvim/lua/plugins/astrocore.lua
@@ -3,75 +3,62 @@ return {
   "AstroNvim/astrocore",
   lazy = false,
   priority = 10000,
+
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
     features = {
-      autopairs = true,
-      cmp = true,
-      diagnostics_mode = 3,
-      highlighturl = true,
+      -- Disable notifications
       notifications = false,
     },
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+      opt = {
+        -- Disable swap files
+        swapfile = false,
 
-        -- navigate buffer tabs with `H` and `L`
-        L = {
-          function()
-            require("astrocore.buffer").nav(vim.v.count1)
-          end,
-          desc = "Next buffer",
-        },
-        H = {
-          function()
-            require("astrocore.buffer").nav(-vim.v.count1)
-          end,
-          desc = "Previous buffer",
+        -- The maximum width of the text being inserted
+        textwidth = 100,
+
+        -- Highlight the line after the textwidth
+        colorcolumn = "+1",
+
+        -- Enable soft wrapping.
+        wrap = true,
+
+        -- Round to multiples of the shift width
+        shiftround = true,
+
+        -- Indent by two spaces when wrapping
+        breakindentopt = "shift:2",
+
+        -- Enable spell checking
+        spell = true,
+
+        -- Display extra whitespace characters
+        list = true,
+
+        -- Define the whitespace characters to display.
+        listchars = {
+          tab = "»·",
+          trail = "·",
+          nbsp = "·",
         },
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(function(bufnr)
-              require("astrocore.buffer").close(bufnr)
-            end)
-          end,
-          desc = "Pick to close",
-        },
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        -- quick save
-        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-      },
-      t = {
-        -- setting a mapping to false will disable it
-        -- ["<esc>"] = false,
+        -- Automatically :write before running commands
+        autowrite = true,
+
+        -- Only join with one space between words
+        joinspaces = false,
+
+        -- Ensure there's always a little space above and below the current line
+        scrolloff = 2,
+
+        -- Prevent annoying horizontal scrolling
+        startofline = false,
+
+        -- Don't use the system clipboard.
+        clipboard = {},
       },
     },
+    mappings = {},
   },
 }
