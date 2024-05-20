8bed4f8565e138b3407bc4fb3f4b2b177d2fa818
Landon Schropp <schroppl@gmail.com>
2024-05-20 00:25:22 -0500

    Remove custom null-ls configuration
    Instead of configuring null-ls myself, I'm going to try to rely on the
    AstroCommunity configuration for my main languages. In my comments for
    mason-null-ls, I had previously disabled the default behavior because I
    was having issues with it, so this might backfile, but it seems worth a
    try.

--- config/nvim/lua/user/plugins/mason-null-ls.lua
+++
@@ -1,6 +0,0 @@
--- This plugin has been causing me headaches in how it configures the language servers. I only use a
--- few language servers, so the rest seem redundant, and I don't usually want them to auto start.
-return {
-  "jay-babu/mason-null-ls.nvim",
-  enabled = false,
-}
