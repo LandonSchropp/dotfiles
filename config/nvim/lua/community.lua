050cdb5f4a9817e03a36e001a32d724c2008ff66
Landon Schropp <schroppl@gmail.com>
2024-05-19 22:56:26 -0500

    Import previous community configuration

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -1,5 +1,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
 }
