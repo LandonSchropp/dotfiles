4aa022a3cf6ada924275a888e12850defb14f7ad
Landon Schropp <schroppl@gmail.com>
2024-05-20 22:48:17 -0500

    Move/remove remaining plugins

--- config/nvim/lua/user/plugins/typescript.lua
+++
@@ -1,9 +0,0 @@
-local extend = require("user.utilities.table").extend
-
-return {
-  "jose-elias-alvarez/typescript.nvim",
-  opts = function()
-    local opts = require("astronvim.utils.lsp").config("tsserver")
-    return { server = opts }
-  end,
-}
