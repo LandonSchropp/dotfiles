e40dbde6f6411780ee8b9cba6fa2bb0a8b1f8ab2
Landon Schropp <schroppl@gmail.com>
2024-05-20 21:12:27 -0500

    Remove unneeded plugins

--- config/nvim/lua/user/plugins/heirline.lua
+++
@@ -1,6 +0,0 @@
--- Heirline is way more trouble than it's worth. Because everything is super custom, there's almost
--- no configuration options available. Instead, I'm opting to use purpose-built plugins for this
--- functionality instead.
-return {
-  "rebelot/heirline.nvim",
-}
