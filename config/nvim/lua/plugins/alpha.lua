d86c2add3a2b306cf3a68af6b9dcf898f7c5c591
Landon Schropp <schroppl@gmail.com>
2024-07-10 22:36:47 -0500

    Clean up Alpha config

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -1,10 +1,8 @@
 return {
   "goolord/alpha-nvim",
-  opts = function()
-    local dashboard = require("alpha.themes.dashboard")
-
-    -- Define a nice header.
-    dashboard.section.header.val = {
+  opts = function(_, opts)
+    -- Redefine the header.
+    opts.section.header.val = {
       " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
       " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
       " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
@@ -14,30 +12,30 @@ return {
     }
 
     -- Redefine the buttons.
-    dashboard.section.buttons.val = {
-      dashboard.button("<Leader>n", "  New File"),
-      dashboard.button("<Leader>ff", "  Find File"),
-      dashboard.button("<Leader>fr", "  Find and Replace"),
-      dashboard.button("<Leader>fg", "󰈭  Grep"),
-      dashboard.button("<Leader>fR", "󰈙  Recents"),
-      dashboard.button("<Leader>Sl", "  Last Session"),
+    opts.section.buttons.val = {
+      opts.button("<Leader>n", "  New File"),
+      opts.button("<Leader>ff", "󰱽  Find File"),
+      opts.button("<Leader>fr", "  Find and Replace"),
+      opts.button("<Leader>fg", "  Grep"),
     }
 
-    -- Shrink the padding
-    dashboard.section.buttons.opts.spacing = 1
-    dashboard.opts.layout[3].val = 1
+    -- Shrink the padding between the sections
+    opts.section.buttons.opts.spacing = 1
+    opts.opts.layout[3].val = 1
+    opts.opts.layout[5].val = 0
 
     -- Determine the height of the content.
-    local content_height = #dashboard.section.header.val
-      + 1
-      + (#dashboard.section.buttons.val * 2 - 1)
-      + 1
-      + 1
 
     -- Vertically center the layout.
     -- TODO: Figure out how to do this dynamically.
-    dashboard.opts.layout[1].val = vim.fn.floor((vim.fn.winheight(0) - content_height) / 2)
+    local content_height = #opts.section.header.val + (#opts.section.buttons.val * 2 - 1) + 3
+    opts.opts.layout[1].val = vim.fn.floor((vim.fn.winheight(0) - content_height + 2) / 2)
 
-    return dashboard
+    -- TODO: Figure out why the highlights disappeared and fix them.
+    opts.section.header.opts.hl = "AlphaHeader"
+    opts.section.buttons.opts.hl = "AlphaButtons"
+    opts.section.footer.opts.hl = "AlphaFooter"
+
+    return opts
   end,
 }
