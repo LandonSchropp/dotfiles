2e96d12f53898acc949a913402bf3bca0c5149bb
Landon Schropp <schroppl@gmail.com>
2024-09-12 23:04:07 -0700

    Configure render-markdown.nvim checkboxes

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -9,6 +9,43 @@ return {
     bullet = {
       icons = { "•" },
     },
+    checkbox = {
+      checked = {
+        icon = "",
+      },
+      unchecked = {
+        icon = "",
+      },
+      custom = {
+        -- This must be included to override the default todo value, which interferes with the
+        -- custom cancelled value since it has the same `raw`.
+        todo = { raw = "[-]", rendered = "󰜺", highlight = "RenderMarkdownCancelled" },
+
+        -- Standard icons
+        incomplete = { raw = "[/]", rendered = "", highlight = "RenderMarkdownIncomplete" },
+        cancelled = { raw = "[-]", rendered = "󰜺", highlight = "RenderMarkdownCancelled" },
+        forwarded = { raw = "[>]", rendered = "", highlight = "RenderMarkdownForwarded" },
+        scheduled = { raw = "[<]", rendered = "󰸘", highlight = "RenderMarkdownScheduled" },
+
+        -- Non-standard extra icons
+        question = { raw = "[?]", rendered = "?", highlight = "RenderMarkdownQuestion" },
+        important = { raw = "[!]", rendered = "", highlight = "RenderMarkdownImportant" },
+        star = { raw = "[*]", rendered = "", highlight = "RenderMarkdownStar" },
+        quote = { raw = '["]', rendered = "", highlight = "RenderMarkdownQuote" },
+        location = { raw = "[l]", rendered = "", highlight = "RenderMarkdownLocation" },
+        bookmark = { raw = "[b]", rendered = "", highlight = "RenderMarkdownBookmark" },
+        information = { raw = "[i]", rendered = "", highlight = "RenderMarkdownInformation" },
+        savings = { raw = "[S]", rendered = "", highlight = "RenderMarkdownSavings" },
+        idea = { raw = "[I]", rendered = "", highlight = "RenderMarkdownIdea" },
+        pros = { raw = "[p]", rendered = "", highlight = "RenderMarkdownPros" },
+        cons = { raw = "[c]", rendered = "", highlight = "RenderMarkdownCons" },
+        fire = { raw = "[f]", rendered = "", highlight = "RenderMarkdownFire" },
+        key = { raw = "[k]", rendered = "", highlight = "RenderMarkdownKey" },
+        win = { raw = "[w]", rendered = "󱁖", highlight = "RenderMarkdownWin" },
+        up = { raw = "[u]", rendered = "󰔵", highlight = "RenderMarkdownUp" },
+        down = { raw = "[d]", rendered = "󰔳", highlight = "RenderMarkdownDown" },
+      },
+    },
     quote = {
       icon = "▎",
     },
