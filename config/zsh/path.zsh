5976eaba2e0555134f694d8b3ef803107dc6872c
Landon Schropp <schroppl@gmail.com>
2024-10-18 23:16:37 -0700

    Rearrange the path commands

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -6,11 +6,6 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
-# Add the local bin paths to the system path. This isn't included in the .zshenv file because it
-# *must* be done after rbenv and fnm have loaded. Since we likely don't want relative paths in
-# non-shell instances of zsh anyway, this is probably better anyway.
-export PATH="./bin:./node_modules/.bin:$PATH"
-
 # direnv
 eval "$(direnv hook zsh)"
 
@@ -25,18 +20,22 @@ export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 [ -s "/Users/landon/.bun/_bun" ] && source "/Users/landon/.bun/_bun"
 
-# Homebrew
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
-fi
-
 # Rust
 if test -f "$HOME/.cargo/env"; then
   source "$HOME/.cargo/env"
 fi
 
-# Add my custom bin scripts to the system path. This must go at the bottom to have a higher
-# precedence than other directories in the path.
+# Homebrew
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
+
+# Add my custom bin scripts to the path. This must go at the bottom to have a higher precedence than
+# other directories in the path.
 export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="$HOME/.dotfiles/bin/gcloud:$PATH"
 export PATH="$HOME/.dotfiles/bin/git:$PATH"
+
+# Add local bin scripts to the path
+export PATH="./bin:$PATH"
+export PATH="./node_modules/.bin:$PATH"
