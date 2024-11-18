68f2bf3aa8384f0834c977ab084bceefed08a611
Landon Schropp <schroppl@gmail.com>
2024-11-17 23:04:53 -0800

    Restore notify.lua configuration
    I previously removed this in e40dbde, but it turns out that this is
    necessary to disable nvim-notify. Otherwise, the plugin will hang around
    and cause issues.

---
+++ config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,7 @@
+-- Don't delete this! This is necessary to disable nvim-notify—using the `notifications = false` in
+-- the astrocore configuration is insufficient by itself.
+return {
+  "rcarriga/nvim-notify",
+  enabled = false,
+}
+
