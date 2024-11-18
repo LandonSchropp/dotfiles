4a54eb5e77b2fdba57cc29a3cf29460cd012184b
Landon Schropp <schroppl@gmail.com>
2024-11-17 23:04:18 -0800

    Fix the LuaSnip configuration
    This has changed since I last configured AstroNvim.

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -2,7 +2,7 @@ return {
   "L3MON4D3/LuaSnip",
   config = function(...)
     -- Call the default AstroNvim LuaSnip configuration.
-    require("plugins.configs.luasnip")(...)
+    require("astronvim.plugins.configs.luasnip")(plugin, opts)
 
     -- Load my custom snippets.
     require("luasnip.loaders.from_lua").load({ paths = { "./lua/snippets" } })
