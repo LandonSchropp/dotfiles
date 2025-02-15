96f69ed610bc80a14a5918b58437cbd085797caf
Landon Schropp <schroppl@gmail.com>
2025-02-14 18:40:06 -0800

    Rename RSpec commands

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -1,5 +1,5 @@
 return {
-  GenerateRSpecCommandsFromOutput = require("commands.generate-rspec-commands-from-output"),
-  GenerateRSpecCommandsFromHarness = require("commands.generate-rspec-commands-from-harness"),
+  RSpecCommandsFromOutput = require("commands.generate-rspec-commands-from-output"),
+  RSpecCommandsFromHarness = require("commands.generate-rspec-commands-from-harness"),
   ReformatToTwoSpaces = require("commands.reformat-to-two-spaces"),
 }
