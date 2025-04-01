e44e990554ae7cc27b349316f53974a919c8ee60
Landon Schropp <schroppl@gmail.com>
2025-03-31 18:35:30 -0700

    Move the picker close behavior to the correct spot

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -8,6 +8,14 @@ return {
         -- Make the picker take up the full window by default.
         fullscreen = true,
       },
+      win = {
+        -- Close the picker on escape instead of going into normal mode.
+        input = {
+          keys = {
+            ["<Esc>"] = { "close", mode = { "n", "i" } },
+          },
+        },
+      },
     },
 
     -- Customize the dashboard.
