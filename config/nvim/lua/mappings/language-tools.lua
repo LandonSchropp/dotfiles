64f5f28ac3fdb8c48111a5cc8f6e8b37ec6b1d18
Landon Schropp <schroppl@gmail.com>
2025-04-02 21:12:27 -0700

    Configure Luasnip choice selection shortcuts

--- config/nvim/lua/mappings/language-tools.lua
+++ config/nvim/lua/mappings/language-tools.lua
@@ -1,14 +1,34 @@
 local extend = require("utilities.table").extend
 
-return function(mappings)
+local function generate_documentation()
+  require("neogen").generate()
+end
+
+local function next_choice()
+  local luasnip = require("luasnip")
+
+  if luasnip.choice_active() then
+    luasnip.change_choice(1)
+  end
+end
+
+local function previous_choice()
+  local luasnip = require("luasnip")
+
+  if luasnip.choice_active() then
+    luasnip.change_choice(-1)
+  end
+end
+
+return function(_)
   return {
     n = {
-      ["<Leader>ld"] = {
-        function()
-          require("neogen").generate()
-        end,
-        desc = "Generate documentation",
-      },
+      ["<Leader>ld"] = { generate_documentation, desc = "Generate documentation" },
+    },
+    i = {
+      ["<C-e>"] = { next_choice, desc = "Next choice" },
+      ["<C-n>"] = { next_choice, desc = "Next choice" },
+      ["<C-p>"] = { previous_choice, desc = "Previous choice" },
     },
   }
 end
