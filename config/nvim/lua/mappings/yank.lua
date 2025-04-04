39d3c7aa5b7b907ad376f3badfd53d64124625fd
Landon Schropp <schroppl@gmail.com>
2025-04-03 22:25:59 -0700

    Fix the gy and gp normal mode mappings

--- config/nvim/lua/mappings/yank.lua
+++ config/nvim/lua/mappings/yank.lua
@@ -2,8 +2,8 @@ return function(_)
   -- Create more convenient mappings for yanking to and pasting from the system clipboard.
   return {
     n = {
-      gy = { '"*y', desc = "Yank to system clipboard" },
-      gp = { '"*p', desc = "Paste from system clipboard" },
+      gy = { '"*y', desc = "Yank to system clipboard", noremap = true },
+      gp = { '"*p', desc = "Paste from system clipboard", noremap = true },
       gY = { '"*y$', desc = "Yank until end of line to system clipboard" },
     },
     v = {
