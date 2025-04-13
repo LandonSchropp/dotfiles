54380748c9b42341cfcbb25c15664fd0a2e180af
Landon Schropp <schroppl@gmail.com>
2025-04-12 22:51:42 -0700

    Drop nvim-notify config (no longer needed)

--- config/nvim/lua/plugins/notify.lua
+++
@@ -1,7 +0,0 @@
--- Don't delete this! This is necessary to disable nvim-notify—using the `notifications = false` in
--- the astrocore configuration is insufficient by itself.
-return {
-  "rcarriga/nvim-notify",
-  enabled = false,
-}
-
