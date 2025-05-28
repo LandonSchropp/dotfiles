7a78c3efc7bf511164ec88173456ae414d413606
Landon Schropp <schroppl@gmail.com>
2025-05-28 13:48:49 -0700

    Replace flash.nvim with hop.nvim

--- config/nvim/lua/mappings/text.lua
+++ config/nvim/lua/mappings/text.lua
@@ -1,8 +1,19 @@
-return function(_)
+return function(mappings)
   return {
     n = {
       -- Prevent the cursor from jumping when joining lines
       J = { "mzJ`z", desc = "Join" },
+
+      -- Hop.nvim
+      ["s"] = false,
+      ["<S-s>"] = false,
+      ["\\"] = mappings.n.s,
+    },
+    v = {
+      -- Hop.nvim
+      ["s"] = false,
+      ["<S-s>"] = false,
+      ["\\"] = mappings.v.s,
     },
   }
 end
