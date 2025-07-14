40fe7f7e189c2ab16bbbff2c33fa41745416bf34
Landon Schropp <schroppl@gmail.com>
2025-07-13 23:23:34 -0500

    Add recording component to lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -17,6 +17,14 @@ local function format_search_count(search_count)
   return search_count:gsub("[[%]]", "")
 end
 
+local function recording()
+  if vim.fn.reg_recording() == "" then
+    return ""
+  end
+
+  return "recording " .. vim.fn.reg_recording()
+end
+
 -- A nicer status line.
 ---@type LazySpec
 return {
@@ -40,6 +48,7 @@ return {
           { "filetype", left = 0,   right = 0 },
         },
         lualine_x = {
+          { recording, icon = "", color = "Constant" },
           { "searchcount", icon = "", color = "Function", fmt = format_search_count },
           { "diff" },
         },
