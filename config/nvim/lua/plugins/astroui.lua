fed591ee4edc1f1ee16670302f08391c9d551d41
Landon Schropp <schroppl@gmail.com>
2025-07-19 14:24:35 -0500

    Style markdown tables

--- config/nvim/lua/plugins/astroui.lua
+++ config/nvim/lua/plugins/astroui.lua
@@ -4,6 +4,18 @@
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
+  init = function()
+    -- TODO: The astroui highlight configuration isn't quite working with highlight groups. This is
+    -- a temporary workaround until that issue is resolved.
+    --
+    -- https://github.com/AstroNvim/astroui/issues/61
+    vim.schedule(function()
+      -- Style the markdown tables
+      local non_text = vim.api.nvim_get_hl(0, { name = "NonText" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownTableHead", { fg = non_text.fg })
+      vim.api.nvim_set_hl(0, "RenderMarkdownTableRow", { fg = non_text.fg })
+    end)
+  end,
   ---@type AstroUIOpts
   opts = {
     colorscheme = "tokyonight-storm",
