9991f704014a148546b7787aee30f76e300e5136
Landon Schropp <schroppl@gmail.com>
2025-10-13 11:35:58 -0700

    Properly set up the pnpm paths

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -6,14 +6,19 @@ FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 eval "$(direnv hook zsh)"
 
 # mise-en-place
+eval "$(mise activate zsh --shims)"
 eval "$(mise activate zsh)"
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
 
 # thefuck
 eval $(thefuck --alias)
 
-# Add the Claude Code installation location to the path
+# Claude Code
 export PATH="$HOME/.local/bin:$PATH"
 
-# Add local bin scripts to the path
+# Local bin scripts
 export PATH="./bin:$PATH"
 export PATH="./node_modules/.bin:$PATH"
