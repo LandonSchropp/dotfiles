f74f336409addf2ddacfac6912796d785ee60ee8
Landon Schropp <schroppl@gmail.com>
2025-10-18 15:54:50 -0700

    Add full script to window manager

--- config/window-manager/window_manager.rb
+++ config/window-manager/window_manager.rb
@@ -3,26 +3,26 @@ require_relative 'screen'
 require_relative 'configuration'
 require_relative 'layout'
 
-module WindowManager
-  class << self
-    def arrange(profile, workspace)
-      # Get the workspace configuration
-      config = Configuration.find_workspace(profile, workspace)
+# Ensure the correct number of arguments
+unless ARGV.length == 2
+  warn "Usage: #{$PROGRAM_NAME} <profile> <workspace>"
+  exit 1
+end
 
-      # Get all visible windows, filter them to only those in the workspace configuration and sort
-      # them by their order in the configuration
-      windows = Window
-        .visible
-        .filter { config.applications.include?(_1.application) }
-        .sort_by { config.applications.index(_1.application) || config.applications.length }
+# Get the workspace configuration
+config = Configuration.find_workspace(ARGV[0], ARGV[1])
 
-      # Calculate the layout rectangles
-      rectangles = Layout.calculate(config.layout, Screen.visible_rectangle, windows.length)
+# Get all visible windows, filter them to only those in the workspace configuration and sort
+# them by their order in the configuration
+windows = Window
+  .visible
+  .filter { config.applications.include?(_1.application) }
+  .sort_by { config.applications.index(_1.application) || config.applications.length }
 
-      # Apply the layout to each window
-      windows.zip(rectangles).each do |window, rectangle|
-        window.update_position(rectangle)
-      end
-    end
-  end
+# Calculate the layout rectangles
+rectangles = Layout.calculate(config.layout, Screen.visible_rectangle, windows.length)
+
+# Apply the layout to each window
+windows.zip(rectangles).each do |window, rectangle|
+  window.update_position(rectangle)
 end
