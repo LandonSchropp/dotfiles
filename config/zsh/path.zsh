971c904d2b0715df43807c38c43a5cac16c98a06
Landon Schropp <schroppl@gmail.com>
2025-10-18 22:02:29 -0700

    Switch several paths to .zshenv for non-interactive shells

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -1,23 +1,17 @@
+# NOTE: Some essential $PATH entries are set in .zshenv so they're available to non-interactive
+# shells.
+
 # Homebrew
-export PATH="/opt/homebrew/bin:$PATH"
 FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 
 # direnv
 eval "$(direnv hook zsh)"
 
 # mise-en-place
-eval "$(mise activate zsh --shims)"
 eval "$(mise activate zsh)"
-
-# pnpm
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
 
 # thefuck
 eval $(thefuck --alias)
-
-# Claude Code
-export PATH="$HOME/.local/bin:$PATH"
 
 # Local bin scripts
 export PATH="./bin:$PATH"
