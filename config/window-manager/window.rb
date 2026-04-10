7a99701f89f9c9abd4c398ea00d513e8f606e030
Landon Schropp <schroppl@gmail.com>
2026-04-10 09:30:25 -0700

    Extract private from_yabai_window helper in Window class

--- config/window-manager/window.rb
+++ config/window-manager/window.rb
@@ -9,13 +9,7 @@ Window = Data.define(:application, :id, :rectangle) do
 
       windows
         .select { _1['is-visible'] && !_1['is-minimized'] && !_1['is-floating'] }
-        .map do |window|
-          Window.new(
-            application: window['app'],
-            id: window['id'],
-            rectangle: Rectangle.from_frame(window['frame'])
-          )
-        end
+        .map { from_yabai_window(_1) }
     end
 
     def non_visible
@@ -24,19 +18,17 @@ Window = Data.define(:application, :id, :rectangle) do
 
       windows
         .reject { _1['is-visible'] }
-        .map do |window|
-          Window.new(
-            application: window['app'],
-            id: window['id'],
-            rectangle: Rectangle.from_frame(window['frame'])
-          )
-        end
+        .map { from_yabai_window(_1) }
     end
 
     def focused
       json = `yabai -m query --windows --window`.force_encoding('UTF-8')
-      window = JSON.parse(json)
+      from_yabai_window(JSON.parse(json))
+    end
 
+    private
+
+    def from_yabai_window(window)
       Window.new(
         application: window['app'],
         id: window['id'],
