ede5cec8e58d6159d982c1d6576efe32e689d0b4
Landon Schropp <schroppl@gmail.com>
2026-04-22 19:08:02 -0700

    Remove obsolete LuaSnip config and snippet keybindings
    The custom LuaSnip config loaded snippets from a directory that no
    longer exists. Drop it and fall back to the AstroNvim default. Remove
    the choice-navigation keybindings from language-tools since there are no
    snippets to cycle.

--- config/nvim/lua/mappings/language-tools.lua
+++ config/nvim/lua/mappings/language-tools.lua
@@ -1,19 +1,3 @@
-local function next_choice()
-  local luasnip = require("luasnip")
-
-  if luasnip.choice_active() then
-    luasnip.change_choice(1)
-  end
-end
-
-local function previous_choice()
-  local luasnip = require("luasnip")
-
-  if luasnip.choice_active() then
-    luasnip.change_choice(-1)
-  end
-end
-
 local function next_diagnostic()
   vim.diagnostic.jump({ count = 1, float = true })
 end
@@ -27,11 +11,6 @@ return function(_)
     n = {
       ["]]"] = { next_diagnostic, desc = "Next diagnostic" },
       ["[["] = { previous_diagnostic, desc = "Previous diagnostic" },
-    },
-    i = {
-      ["<C-e>"] = { next_choice, desc = "Next choice" },
-      ["<C-n>"] = { next_choice, desc = "Next choice" },
-      ["<C-p>"] = { previous_choice, desc = "Previous choice" },
     },
   }
 end
