0e4337fbd1cb77dfc03e49b4b671847fcded263e
Landon Schropp <schroppl@gmail.com>
2026-05-11 14:16:50 -0700

    Restore cursor visibility before each prompt

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,3 +1,7 @@
 # Set the prompt to Starship
 eval "$(starship init zsh)"
 autoload -U promptinit; promptinit
+
+# Restore cursor visibility before each prompt in case a program hid it
+_restore_cursor() { printf '\e[?25h'; }
+precmd_functions+=(_restore_cursor)
