ca557727aa3ee6fb8d219138c3f61def0db5e19f
Landon Schropp <schroppl@gmail.com>
2026-07-25 10:23:54 -0700

    Restore the Ruby describe and context snippets

---
+++ config/nvim/lua/snippets/ruby.lua
@@ -0,0 +1,56 @@
+local luasnip = require("luasnip")
+local format = require("luasnip.extras.fmt").fmta
+local snippet_utils = require("utilities.snippet")
+
+local snippet_with_stores = snippet_utils.snippet_with_stores
+local string_node = snippet_utils.string_node
+
+local choice = luasnip.c
+local restore = luasnip.r
+
+---Creates a node for Ruby blocks.
+---@param jump_index number The index the node should use for jumping.
+---@param key string The key of the restore node to use for the node's content.
+---@param types ("inline" | "multi-line")[] A list of types to allow for the block node. Defaults to
+---  `{ "multi-line", "inline" }`
+---@return unknown
+local block_node = function(jump_index, key, types)
+  local choices = vim.tbl_map(function(type)
+    if type == "inline" then
+      return format("{ <> }", { restore(1, key) })
+    elseif type == "multi-line" then
+      return format("do\n  <>\nend", { restore(1, key) })
+    else
+      error("An unrecognized type '" .. type .. "' was provided to block_node.")
+    end
+  end, types)
+
+  return choice(jump_index, choices, { restore_cursor = true })
+end
+
+--- Returns true if the current buffer is a spec file.
+--- @return boolean True if the current buffer is a spec file, false otherwise.
+local function is_spec()
+  return vim.fn.expand("%:t"):match("_spec%.rb$") ~= nil
+end
+
+return {
+  snippet_with_stores(
+    "describe",
+    format("describe <> <>", {
+      string_node(1, "description", { "double", "single", "bare" }),
+      block_node(2, "text", { "multi-line" }),
+    }),
+    { "description", "text" },
+    { condition = is_spec, show_condition = is_spec }
+  ),
+  snippet_with_stores(
+    "context",
+    format("context <> <>", {
+      string_node(1, "description"),
+      block_node(2, "text", { "multi-line" }),
+    }),
+    { "description", "text" },
+    { condition = is_spec, show_condition = is_spec }
+  ),
+}
