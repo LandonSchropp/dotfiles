84b4dfbb8a1fd7df548dda7767fa9d20414b99d9
Landon Schropp <schroppl@gmail.com>
2026-07-26 14:34:24 -0700

    Include TypeScript snippets in tsx files

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -11,5 +11,8 @@ return {
     require("luasnip.loaders.from_lua").lazy_load({
       paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
     })
+
+    -- Include TypeScript snippets in tsx files.
+    require("luasnip").filetype_extend("typescriptreact", { "typescript" })
   end,
 }
